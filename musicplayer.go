0cdde53ab86edd68c5466a3c4d1f94d78a1af245
kraxarn <me@kraxarn.com>
2020-02-12 22:18:35 +0100

    Use QDbus instead

--- musicplayer.go
+++ musicplayer.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/godbus/dbus"
+	"github.com/therecipe/qt/core"
+	"github.com/therecipe/qt/dbus"
 )
 
 type MusicPlayer struct {
-	bus *dbus.Conn
-	obj dbus.BusObject
+	itf *dbus.QDBusInterface
 }
 
 func NewMusicPlayer() *MusicPlayer {
@@ -18,22 +18,19 @@ func NewMusicPlayer() *MusicPlayer {
 }
 
 func (mp *MusicPlayer) Connect() {
-	var err error
-	// Connect to bus
-	if mp.bus != nil {
-		mp.bus.Close()
-		mp.bus = nil
-	}
-	mp.bus, err = dbus.ConnectSessionBus()
-	if err != nil {
-		fmt.Println("error: failed to connect to session bus:", err)
-		return
-	}
-	// Prepare the object to media player
-	mp.obj = mp.bus.Object("org.mpris.MediaPlayer2.spotifyd", "/org/mpris/MediaPlayer2")
+	mp.itf = dbus.NewQDBusInterface2(
+		"org.mpris.MediaPlayer2.spotifyd",
+		"/org/mpris/MediaPlayer2",
+		"org.mpris.MediaPlayer2.Player",
+		dbus.QDBusConnection_SessionBus(), nil)
 }
 
 func (mp *MusicPlayer) Play(trackID string) error {
-	return mp.obj.Call("org.mpris.MediaPlayer2.Player.OpenUri", dbus.FlagNoReplyExpected,
-		fmt.Sprintf("spotify:track:%v", trackID)).Err
+	result := mp.itf.CallWithArgumentList(0, "OpenUri", []*core.QVariant{
+		core.NewQVariant1(fmt.Sprintf("spotify:track:%v", trackID)),
+	})
+	if len(result.ErrorMessage()) > 0 {
+		return fmt.Errorf(result.ErrorMessage())
+	}
+	return nil
 }
