5fc605672cf297e66469db88f9c6dae529229fbf
kraxarn <me@kraxarn.com>
2020-02-13 23:51:58 +0100

    Add status reporting through go dbus

--- musicplayer.go
+++ musicplayer.go
@@ -2,30 +2,46 @@ package main
 
 import (
 	"fmt"
+	godbus "github.com/godbus/dbus"
 	"github.com/therecipe/qt/core"
 	"github.com/therecipe/qt/dbus"
 )
 
 type MusicPlayer struct {
 	itf *dbus.QDBusInterface
+	bus godbus.BusObject
+	isDBus bool
+
+	//metadataMsg *dbus.QDBusMessage
+}
+
+type PlayerStatus struct {
+	TrackID, Title, Artist, PlaybackStatus, ArtUrl string
+	Position, Length int64
 }
 
 func NewMusicPlayer() *MusicPlayer {
 	// Create media player
 	mp := new(MusicPlayer)
 	mp.Connect()
+	mp.isDBus = false
 	return mp
 }
 
 func (mp *MusicPlayer) Connect() {
 	go func() {
 		fmt.Println("connecting to d-bus...")
+		// QDBus
 		mp.itf = dbus.NewQDBusInterface2(
 			"org.mpris.MediaPlayer2.spotifyd",
 			"/org/mpris/MediaPlayer2",
 			"org.mpris.MediaPlayer2.Player",
 			dbus.QDBusConnection_SessionBus(), nil)
+		// Go DBus
+		conn, _ := godbus.ConnectSessionBus()
+		mp.bus = conn.Object("org.mpris.MediaPlayer2.spotifyd", "/org/mpris/MediaPlayer2")
 		fmt.Println("connected to d-bus")
+		mp.isDBus = true
 	}()
 }
 
@@ -37,4 +53,23 @@ func (mp *MusicPlayer) Play(trackID string) error {
 		return fmt.Errorf(result.ErrorMessage())
 	}
 	return nil
+}
+
+func (mp *MusicPlayer) Status() *PlayerStatus {
+	// Get status ready
+	status := new(PlayerStatus)
+	// Get metadata
+	metadataVariant, _ := mp.bus.GetProperty("org.mpris.MediaPlayer2.Player.Metadata")
+	metadata := metadataVariant.Value().(map[string]godbus.Variant)
+	if len(metadata) <= 0 {
+		return nil
+	}
+	// Set values
+	status.Artist = metadata["xesam:artist"].Value().([]string)[0]
+	status.ArtUrl = metadata["mpris:artUrl"].Value().(string)
+	status.Length = metadata["mpris:length"].Value().(int64)
+	status.Title  = metadata["xesam:title"].Value().(string)
+	// Get position
+	status.Position = mp.itf.Property("Position").ToLongLong(nil)
+	return status
 }
