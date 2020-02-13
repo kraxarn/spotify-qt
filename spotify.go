9b9bfabb87eb6fb5bb2f304c7729222fc7a616c6
kraxarn <me@kraxarn.com>
2020-02-13 01:00:15 +0100

    Load all tracks in playlist

--- spotify.go
+++ spotify.go
@@ -28,21 +28,36 @@ type SpotifyPlaylist struct {
 	Tracks			map[string]interface{}
 }
 
-func (pl *SpotifyPlaylist) LoadTracks(spt *Spotify) ([]SpotifyTrack, error) {
+func (pl *SpotifyPlaylist) LoadTracksFromUrl(tracks []SpotifyTrack, url string, offset int, spt *Spotify) error {
 	// Load tracks from api
-	tracks, err := spt.Request(pl.Tracks["href"].(string))
+	current, err := spt.Request(url)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	// Load tracks
-	trackList := make([]SpotifyTrack, int(pl.Tracks["total"].(float64)))
-	for i, track := range tracks["items"].([]interface{}) {
+	// Load for this url
+	items := current["items"].([]interface{})
+	for i, track := range items {
 		t := track.(map[string]interface{})
-		trackList[i] = SpotifyTrack{
+		tracks[offset+i] = SpotifyTrack{
 			AddedAt: time.Time{},
 			IsLocal: t["is_local"].(bool),
 			Track:   t["track"].(map[string]interface{}),
 		}
+	}
+	// Check if there's a next page
+	if current["next"] != nil {
+		if err := pl.LoadTracksFromUrl(tracks, current["next"].(string), offset + len(items), spt); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (pl *SpotifyPlaylist) LoadTracks(spt *Spotify) ([]SpotifyTrack, error) {
+	// Allocate memory for all tracks
+	trackList := make([]SpotifyTrack, int(pl.Tracks["total"].(float64)))
+	if err := pl.LoadTracksFromUrl(trackList, pl.Tracks["href"].(string), 0, spt); err != nil {
+		return nil, err
 	}
 	return trackList, nil
 }
