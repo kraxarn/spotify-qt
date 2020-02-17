5bff7614fc1f267c1f7b2f21bb8ffd7ee5f7d337
kraxarn <me@kraxarn.com>
2020-02-17 21:33:17 +0100

    Setup some stuff as needed

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QCoreApplication>
 
 int main(int argc, char *argv[])
 {
+	// Set name for settings etc.
+	QCoreApplication::setOrganizationName("kraxarn");
+	QCoreApplication::setApplicationName("spotify-qt");
+	// Create Qt application
 	QApplication a(argc, argv);
+	// Spotify auth
+	// TODO
+	// Create main window
 	MainWindow w;
+	// Show window and run application
 	w.show();
-	return a.exec();
+	return QApplication::exec();
 }
