5107f1e89ce96452f5993c7a66bd3ec3a92f525d
kraxarn <me@kraxarn.com>
2020-02-18 23:27:06 +0100

    Fix creation of window

--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,13 @@ int main(int argc, char *argv[])
 	QCoreApplication::setOrganizationName("kraxarn");
 	QCoreApplication::setApplicationName("spotify-qt");
 	// Create Qt application
-	QApplication a(argc, argv);
+	QApplication app(argc, argv);
 	// Spotify auth
 	spt::Spotify spotify;
 	if (!spotify.auth())
 		qFatal("error: authentication failed");
 	// Create main window
-	MainWindow w;
+	MainWindow w(&spotify, &app);
 	// Show window and run application
 	w.show();
 	return QApplication::exec();
