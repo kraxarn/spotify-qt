ce4663e6f076ee0940c1983826d277435dad390e
kraxarn <me@kraxarn.com>
2020-02-21 21:01:25 +0100

    Update for new auth

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include "mainwindow.hpp"
+#include "setupdialog.hpp"
 #include "src/spotify/spotify.hpp"
 
 #include <QApplication>
 #include <QCoreApplication>
+#include <QDesktopServices>
 
 int main(int argc, char *argv[])
 {
@@ -11,10 +13,15 @@ int main(int argc, char *argv[])
 	QCoreApplication::setApplicationName("spotify-qt");
 	// Create Qt application
 	QApplication app(argc, argv);
-	// Spotify auth
+	// Spotify class instance
 	spt::Spotify spotify;
-	if (!spotify.auth())
-		qFatal("error: authentication failed");
+	// First setup window
+	if (Settings().refreshToken().isEmpty() || true)
+	{
+		SetupDialog dialog(&spotify);
+		if (dialog.exec() == QDialog::Rejected)
+			return 0;
+	}
 	// Create main window
 	MainWindow w(&spotify, &app);
 	// Show window and run application
