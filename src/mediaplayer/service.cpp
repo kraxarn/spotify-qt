fde9a50e3abb7cda6ef0347788f94d9262acaf5e
kraxarn <me@kraxarn.com>
2020-04-01 22:40:05 +0200

    Remove base interface

--- src/mediaplayer/service.cpp
+++ src/mediaplayer/service.cpp
@@ -3,13 +3,12 @@
 using namespace mp;
 
 #define SERVICE_NAME "org.mpris.MediaPlayer2.spotify-qt"
-#define SERVICE_INTERFACE "org.mpris.MediaPlayer2"
 #define SERVICE_PATH "/org/mpris/MediaPlayer2"
 
 Service::Service(spt::Spotify *spotify, QObject *parent) : QObject(parent)
 {
 	QDBusConnection::sessionBus().registerService(SERVICE_NAME);
-	QDBusConnection::sessionBus().registerObject(SERVICE_PATH, SERVICE_INTERFACE, this);
+	QDBusConnection::sessionBus().registerObject(SERVICE_PATH, this);
 
 	player 			= new MediaPlayer(spotify, this);
 	playerPlayer	= new MediaPlayerPlayer(spotify, this);
