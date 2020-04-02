efc3a4317eb5af6fb99c16f954e0aae99180240d
kraxarn <me@kraxarn.com>
2020-04-02 23:48:42 +0200

    Reorder to match specification order

--- src/mediaplayer/mediaplayerplayer.hpp
+++ src/mediaplayer/mediaplayerplayer.hpp
@@ -17,16 +17,16 @@ class MediaPlayerPlayer : public QDBusAbstractAdaptor
 	Q_OBJECT
 	Q_CLASSINFO("D-Bus Interface", "org.mpris.MediaPlayer2.Player")
 
-	Q_PROPERTY(bool CanControl READ canControl)
-	Q_PROPERTY(bool CanGoNext READ canControl)
-	Q_PROPERTY(bool CanGoPrevious READ canControl)
-	Q_PROPERTY(bool CanPause READ canControl)
-	Q_PROPERTY(bool CanPlay READ canControl)
-	Q_PROPERTY(bool CanSeek READ canControl)
+	Q_PROPERTY(QString PlaybackStatus READ playbackStatus)
 	Q_PROPERTY(QMap<QString, QVariant> Metadata READ metadata)
 	Q_PROPERTY(double Volume READ getVolume WRITE setVolume)
 	Q_PROPERTY(qint64 Position READ position)
-	Q_PROPERTY(QString PlaybackStatus READ playbackStatus)
+	Q_PROPERTY(bool CanGoNext READ canControl)
+	Q_PROPERTY(bool CanGoPrevious READ canControl)
+	Q_PROPERTY(bool CanPlay READ canControl)
+	Q_PROPERTY(bool CanPause READ canControl)
+	Q_PROPERTY(bool CanSeek READ canControl)
+	Q_PROPERTY(bool CanControl READ canControl)
 
 public:
 	explicit MediaPlayerPlayer(spt::Spotify *spotify, QObject *parent);
@@ -43,13 +43,14 @@ public:
 
 public slots:
 	Q_NOREPLY void Next();
-	Q_NOREPLY void Pause();
-	Q_NOREPLY void Play();
-	Q_NOREPLY void PlayPause();
 	Q_NOREPLY void Previous();
+	Q_NOREPLY void Pause();
+	Q_NOREPLY void PlayPause();
+	Q_NOREPLY void Stop();
+	Q_NOREPLY void Play();
 	Q_NOREPLY void Seek(qint64 offset);
 	Q_NOREPLY void SetPosition(QDBusObjectPath trackId, qint64 position);
-	Q_NOREPLY void Stop();
+	Q_NOREPLY void OpenUri(QString uri);
 
 private:
 	QDBusConnection	dBus;
