cd36fdf849ac69ae21455d39bf8f398dfbbf3cca
kraxarn <me@kraxarn.com>
2020-04-05 20:58:47 +0200

    Fix incorrect signals

--- src/mediaplayer/mediaplayerplayer.hpp
+++ src/mediaplayer/mediaplayerplayer.hpp
@@ -54,16 +54,15 @@ namespace mp
 		bool shuffle() const;
 		void setShuffle(bool value) const;
 
+	signals:
+		void currentSourceChanged(const spt::Playback &playback) const;
+		void totalTimeChanged(const spt::Playback &playback) const;
+		void Seeked(qint64 position);
+		void stateUpdated() const;
+		void seekableChanged(bool seekable) const;
 		void tick(qint64 newPos);
 		void emitMetadataChange() const;
-		void currentSourceChanged(const spt::Playback &playback) const;
-		void stateUpdated() const;
-		void totalTimeChanged(const spt::Playback &playback) const;
-		void seekableChanged(bool seekable) const;
 		void volumeChanged() const;
-
-	signals:
-		void Seeked(qint64 position);
 
 	public slots:
 		void Next() const;
