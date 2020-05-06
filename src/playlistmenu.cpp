0c38220fc8679cff3fcb555b10f9628dbe9de0dc
kraxarn <me@kraxarn.com>
2020-05-06 23:15:33 +0200

    Initial playlist context menu

--- src/playlistmenu.cpp
+++ src/playlistmenu.cpp
@@ -1,26 +1,58 @@
 #include "playlistmenu.hpp"
 
-PlaylistMenu::PlaylistMenu(spt::Spotify &spotify, spt::Playlist playlist, QWidget *parent) : QMenu(parent)
+PlaylistMenu::PlaylistMenu(spt::Spotify &spotify, const spt::Playlist &playlist, QWidget *parent) : QMenu(parent)
 {
 	auto window = dynamic_cast<MainWindow*>(parent);
 	if (window == nullptr)
 		return;
-	addAction(window->getImage("playlistImage", playlist.image), playlist.name)->setEnabled(false);
+	auto playlistName = addAction(window->getImage("playlistImage", playlist.image), playlist.name);
+	QAction::connect(playlistName, &QAction::triggered, [window, playlist](bool checked) {
+		QInputDialog::getText(window, "New name",
+			"Name:", QLineEdit::Normal, playlist.name);
+	});
+	QAction *editDescription = nullptr;
 	if (!playlist.description.isEmpty())
-		addAction(playlist.description)->setEnabled(false);
+		editDescription = addAction(playlist.description);
 
 	auto tracks = window->playlistTracks(playlist.id);
 	auto duration = 0;
 	for (auto &track : tracks)
 		duration += track.duration;
 	auto minutes = duration / 1000 / 60;
-
-	addAction(QString("%1 tracks, %2%3 m")
-		.arg(tracks.length())
-		.arg(minutes >= 60 ? QString("%1 h ").arg(minutes / 60) : QString())
-		.arg(minutes % 60))->setEnabled(false);
+	if (tracks.length() > 1)
+		addAction(QString("%1 tracks, %2%3 m")
+			.arg(tracks.length())
+			.arg(minutes >= 60 ? QString("%1 h ").arg(minutes / 60) : QString())
+			.arg(minutes % 60))->setEnabled(false);
 
 	addSeparator();
+	auto playShuffle = addAction(Icon::get("media-playlist-shuffle"), "Shuffle play");
+	QAction::connect(playShuffle, &QAction::triggered, [tracks, playlist, &spotify, window](bool checked) {
+		auto initial = tracks.at(QRandomGenerator::global()->bounded(tracks.length()));
+		auto status = spotify.playTracks(
+			QString("spotify:track:%1").arg(initial.id),
+			QString("spotify:playlist:%1").arg(playlist.id));
+		if (status.isEmpty())
+			status = spotify.setShuffle(true);
+		if (!status.isEmpty())
+			window->setStatus(status, true);
+	});
+
+	auto editPlaylist = addMenu(Icon::get("document-edit"), "Edit");
+	if (editDescription == nullptr)
+		editDescription = editPlaylist->addAction("Add description");
+	QAction::connect(editDescription, &QAction::triggered, [window, playlist](bool checked) {
+		auto text = TextEditDialog::getText("New description",
+			"Description:", playlist.description, window);
+		qDebug() << text;
+	});
+	auto editPublic = editPlaylist->addAction("Public");
+	editPublic->setCheckable(true);
+	editPublic->setChecked(playlist.isPublic);
+	auto editCollaborative = editPlaylist->addAction("Collaborative");
+	editCollaborative->setCheckable(true);
+	editCollaborative->setChecked(playlist.collaborative);
+
 	auto share = addMenu(Icon::get("document-share"), "Share");
 	auto sharePlaylist = share->addAction("Copy playlist link");
 	QAction::connect(sharePlaylist, &QAction::triggered, [window, playlist](bool checked) {
--- src/playlistmenu.hpp
+++ src/playlistmenu.hpp
@@ -2,14 +2,17 @@
 
 #include "spotify/playlist.hpp"
 #include "mainwindow.hpp"
+#include "texteditdialog.hpp"
 
 #include <QMenu>
 #include <QUrl>
+#include <QRandomGenerator>
+#include <QInputDialog>
 
 class PlaylistMenu : public QMenu
 {
 	Q_OBJECT
 
 public:
-	PlaylistMenu(spt::Spotify &spotify, spt::Playlist playlist, QWidget *parent = nullptr);
+	PlaylistMenu(spt::Spotify &spotify, const spt::Playlist &playlist, QWidget *parent = nullptr);
 };
