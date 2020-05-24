c49f8c058455b190c84ed17b0eef68b042734f75
kraxarn <me@kraxarn.com>
2020-05-24 13:57:46 +0200

    Remove setters for legacy settings

--- src/settings.cpp
+++ src/settings.cpp
@@ -174,19 +174,9 @@ QString Settings::accessToken()
 	return settings->value("AccessToken").toString();
 }
 
-void Settings::setAccessToken(QString &value)
-{
-	settings->setValue("AccessToken", value);
-}
-
 QString Settings::refreshToken()
 {
 	return settings->value("RefreshToken").toString();
-}
-
-void Settings::setRefreshToken(QString &value)
-{
-	settings->setValue("RefreshToken", value);
 }
 
 QString Settings::clientId()
@@ -194,19 +184,9 @@ QString Settings::clientId()
 	return settings->value("ClientId").toString();
 }
 
-void Settings::setClientId(const QString &value)
-{
-	settings->setValue("ClientId", value);
-}
-
 QString Settings::clientSecret()
 {
 	return settings->value("ClientSecret").toString();
-}
-
-void Settings::setClientSecret(const QString &value)
-{
-	settings->setValue("ClientSecret", value);
 }
 
 QString Settings::style()
@@ -214,19 +194,9 @@ QString Settings::style()
 	return settings->value("Style").toString();
 }
 
-void Settings::setStyle(const QString &value)
-{
-	settings->setValue("Style", value);
-}
-
 QString Settings::sptPath()
 {
 	return settings->value("Spotify/Path").toString();
-}
-
-void Settings::setSptPath(const QString &value)
-{
-	settings->setValue("Spotify/Path", value);
 }
 
 bool Settings::sptStartClient()
@@ -234,19 +204,9 @@ bool Settings::sptStartClient()
 	return settings->value("Spotify/StartClient").toBool();
 }
 
-void Settings::setSptStartClient(bool value)
-{
-	settings->setValue("Spotify/StartClient", value);
-}
-
 QString Settings::sptUser()
 {
 	return settings->value("Spotify/Username").toString();
-}
-
-void Settings::setSptUser(const QString &value)
-{
-	settings->setValue("Spotify/Username", value);
 }
 
 int Settings::sptBitrate()
@@ -254,19 +214,9 @@ int Settings::sptBitrate()
 	return settings->value("Spotify/Bitrate").toInt();
 }
 
-void Settings::setSptBitrate(int value)
-{
-	settings->setValue("Spotify/Bitrate", value);
-}
-
 bool Settings::pulseVolume()
 {
 	return settings->value("PulseVolume").toBool();
-}
-
-void Settings::setPulseVolume(bool value)
-{
-	settings->setValue("PulseVolume", value);
 }
 
 QString Settings::lastPlaylist()
@@ -274,19 +224,9 @@ QString Settings::lastPlaylist()
 	return settings->value("LastPlaylist").toString();
 }
 
-void Settings::setLastPlaylist(const QString &value)
-{
-	settings->setValue("LastPlaylist", value);
-}
-
 Settings::Palette Settings::stylePalette()
 {
 	return (Settings::Palette) settings->value("StylePalette").toInt();
-}
-
-void Settings::setStylePalette(Settings::Palette value)
-{
-	settings->setValue("StylePalette", value);
 }
 
 bool Settings::mediaController()
@@ -297,11 +237,6 @@ bool Settings::mediaController()
 	auto defVal = false;
 #endif
 	return settings->value("MediaController", defVal).toBool();
-}
-
-void Settings::setMediaController(bool value)
-{
-	settings->setValue("MediaController", value);
 }
 
 bool Settings::darkTheme()
@@ -321,11 +256,6 @@ bool Settings::sptPlaybackOrder()
 	return settings->value("SpotifyPlaybackOrder", false).toBool();
 }
 
-void Settings::setSptPlaybackOrder(bool value)
-{
-	settings->setValue("SpotifyPlaybackOrder", value);
-}
-
 QList<int> Settings::hiddenSongHeaders()
 {
 	QList<int> list;
@@ -338,36 +268,9 @@ QList<int> Settings::hiddenSongHeaders()
 	return list;
 }
 
-void Settings::setHiddenSongHeaders(const QList<int> &values)
-{
-	QStringList list;
-	for (auto &value : values)
-		list.append(QString::number(value));
-	settings->setValue("HiddenSongHeaders", list.join(','));
-}
-
-void Settings::removeHiddenSongHeader(int id)
-{
-	auto headers = hiddenSongHeaders();
-	headers.removeOne(id);
-	setHiddenSongHeaders(headers);
-}
-
-void Settings::addHiddenSongHeader(int id)
-{
-	auto headers = hiddenSongHeaders();
-	headers.append(id);
-	setHiddenSongHeaders(headers);
-}
-
 bool Settings::trayIcon()
 {
 	return settings->value("TrayIcon", false).toBool();
-}
-
-void Settings::setTrayIcon(bool value)
-{
-	settings->setValue("TrayIcon", value);
 }
 
 bool Settings::trayNotifications()
@@ -375,19 +278,9 @@ bool Settings::trayNotifications()
 	return settings->value("TrayNotifications", false).toBool();
 }
 
-void Settings::setTrayNotifications(bool value)
-{
-	settings->setValue("TrayNotifications", value);
-}
-
 bool Settings::trayLightIcon()
 {
 	return settings->value("TrayLightIcon", false).toBool();
-}
-
-void Settings::setTrayLightIcon(bool value)
-{
-	settings->setValue("TrayLightIcon", value);
 }
 
 QHeaderView::ResizeMode Settings::songHeaderResizeMode()
@@ -396,19 +289,9 @@ QHeaderView::ResizeMode Settings::songHeaderResizeMode()
 		"SongHeaderResizeMode", QHeaderView::ResizeToContents).toInt();
 }
 
-void Settings::setSongHeaderResizeMode(QHeaderView::ResizeMode value)
-{
-	settings->setValue("SongHeaderResizeMode", value);
-}
-
 int Settings::songHeaderSortBy()
 {
 	return settings->value("SongHeaderSortBy", -1).toInt();
-}
-
-void Settings::setSongHeaderSortBy(int value)
-{
-	settings->setValue("SongHeaderSortBy", value);
 }
 
 int Settings::refreshInterval()
@@ -416,19 +299,9 @@ int Settings::refreshInterval()
 	return settings->value("RefreshInterval", 3).toInt();
 }
 
-void Settings::setRefreshInterval(int value)
-{
-	settings->setValue("RefreshInterval", value);
-}
-
 bool Settings::sptGlobalConfig()
 {
 	return settings->value("Spotify/GlobalConfig", false).toBool();
-}
-
-void Settings::setSptGlobalConfig(bool value)
-{
-	settings->setValue("Spotify/GlobalConfig", value);
 }
 
 QString Settings::lastVersion()
@@ -436,19 +309,9 @@ QString Settings::lastVersion()
 	return settings->value("LastVersion").toString();
 }
 
-void Settings::setLastVersion(const QString &value)
-{
-	settings->setValue("LastVersion", value);
-}
-
 bool Settings::showChangelog()
 {
 	return settings->value("ShowChangelog", true).toBool();
-}
-
-void Settings::setShowChangelog(bool value)
-{
-	settings->setValue("ShowChangelog", value);
 }
 
 bool Settings::useFallbackIcons()
@@ -456,17 +319,7 @@ bool Settings::useFallbackIcons()
 	return settings->value("FallbackIcons", false).toBool();
 }
 
-void Settings::setUseFallbackIcons(bool value)
-{
-	settings->setValue("FallbackIcons", value);
-}
-
 bool Settings::fixedWidthTime()
 {
 	return settings->value("FixedWidthTime", false).toBool();
-}
-
-void Settings::setFixedWidthTime(bool value)
-{
-	settings->setValue("FixedWidthTime", value);
 }
--- src/settings.hpp
+++ src/settings.hpp
@@ -61,86 +61,58 @@ public:
 private:
 
 	QString accessToken();
-	void setAccessToken(QString &value);
 
 	QString refreshToken();
-	void setRefreshToken(QString &value);
 
 	QString clientId();
-	void setClientId(const QString &value);
 
 	QString clientSecret();
-	void setClientSecret(const QString &value);
 
 	QString style();
-	void setStyle(const QString &value);
 
 	bool pulseVolume();
-	void setPulseVolume(bool value);
 
 	QString lastPlaylist();
-	void setLastPlaylist(const QString &value);
 
 	Palette stylePalette();
-	void setStylePalette(Palette value);
 
 	bool mediaController();
-	void setMediaController(bool value);
 
 	bool sptPlaybackOrder();
-	void setSptPlaybackOrder(bool value);
 
 	QList<int> hiddenSongHeaders();
-	void setHiddenSongHeaders(const QList<int> &values);
-	void addHiddenSongHeader(int id);
-	void removeHiddenSongHeader(int id);
 
 	bool trayIcon();
-	void setTrayIcon(bool value);
 
 	bool trayNotifications();
-	void setTrayNotifications(bool value);
 
 	bool trayLightIcon();
-	void setTrayLightIcon(bool value);
 
 	QHeaderView::ResizeMode songHeaderResizeMode();
-	void setSongHeaderResizeMode(QHeaderView::ResizeMode value);
 
 	int songHeaderSortBy();
-	void setSongHeaderSortBy(int value);
 
 	int refreshInterval();
-	void setRefreshInterval(int value);
 
 	QString lastVersion();
-	void setLastVersion(const QString &value);
 
 	bool showChangelog();
-	void setShowChangelog(bool value);
 
 	bool useFallbackIcons();
-	void setUseFallbackIcons(bool value);
 
 	// Spotify settings
 
 	QString sptPath();
-	void setSptPath(const QString &value);
 
 	bool sptStartClient();
-	void setSptStartClient(bool value);
 
 	QString sptUser();
-	void setSptUser(const QString &value);
 
 	int sptBitrate();
-	void setSptBitrate(int value);
 
 	bool sptGlobalConfig();
-	void setSptGlobalConfig(bool value);
 
 	bool fixedWidthTime();
-	void setFixedWidthTime(bool value);
 
 	void load();
 
