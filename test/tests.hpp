48a4094e3b77df0e77635474e56d819b317f5eb2
kraxarn <me@kraxarn.com>
2020-07-29 21:57:19 +0200

    Initial unit testing

---
+++ test/tests.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <QFile>
+#include <QFileInfo>
+#include <QStandardPaths>
+#include <QCoreApplication>
+#include <QDebug>
+#include <QTest>
+
+#include "../src/spotify/spotify.hpp"
+#include "../src/settings.hpp"
