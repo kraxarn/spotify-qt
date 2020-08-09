918b7fe3a5fa2773639f023e6d1e45145572681c
kraxarn <me@kraxarn.com>
2020-08-09 18:59:38 +0200

    New lyrics provider

--- src/lyricsview.cpp
+++ src/lyricsview.cpp
@@ -3,24 +3,27 @@
 LyricsView::LyricsView(const QString &artist, const QString &name, QWidget *parent) : QDockWidget(parent)
 {
 	auto window = (MainWindow*) parent;
-	window->setStatus("Loading lyrics...");
-	auto reply = window->get(QString(
-		"https://vscode-spotify-lyrics.azurewebsites.net/lyrics?artist=%1&title=%2")
-			.arg(artist)
-			.arg(name));
-	auto lyricsText = QString(reply).trimmed();
-	if (lyricsText == "Not found")
+
+	auto reply = window->get(
+		QString("https://lyrics.fandom.com/wiki/%1:%2?action=raw")
+			.arg(format(artist))
+			.arg(format(name)));
+	if (reply.isEmpty())
 	{
 		window->setStatus("Lyrics not found", true);
 		found = false;
 		return;
 	}
-	window->setStatus("Lyrics loaded");
+
+	auto html = QString(reply);
+	auto lyricsStart = html.indexOf("<lyrics>") + 9;
+	html = html.mid(lyricsStart, html.indexOf("</lyrics>") - lyricsStart);
+
 	setWindowTitle(QString("%1 - %2")
 		.arg(artist)
 		.arg(name));
 	auto lyricsView = new QTextEdit(this);
-	lyricsView->setHtml(lyricsText.replace("\n", "<br/>"));
+	lyricsView->setPlainText(html);
 	lyricsView->setReadOnly(true);
 	setWidget(lyricsView);
 	setMinimumWidth(300);
@@ -30,4 +33,11 @@ LyricsView::LyricsView(const QString &artist, const QString &name, QWidget *pare
 bool LyricsView::lyricsFound() const
 {
 	return found;
+}
+
+QString LyricsView::format(const QString &word)
+{
+	return word.left(1).toUpper() +
+		word.right(word.length() - 1).toLower()
+			.replace(' ', '_');
 }
--- src/lyricsview.hpp
+++ src/lyricsview.hpp
@@ -17,6 +17,7 @@ public:
 
 private:
 	bool found;
+	static QString format(const QString &word);
 };
 
 
