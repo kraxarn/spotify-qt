ec87d180ff60cb74327754166f9400ca59c17331
kraxarn <me@kraxarn.com>
2020-08-10 20:22:25 +0200

    Clean up

--- src/dialog/systeminfodialog.cpp
+++ src/dialog/systeminfodialog.cpp
@@ -13,15 +13,17 @@ SystemInfoDialog::SystemInfoDialog(QWidget *parent) : QDialog(parent)
 
 	auto infoAbout = new QLabel(
 		"This information could be useful when reporting bugs. "
-			"Additional information, depending on the type of issue, may be more helpful.", this);
+		"Additional information, depending on the type of issue, may be more helpful.", this);
 	infoAbout->setWordWrap(true);
 	layout->addWidget(infoAbout);
 
 	auto buttons = new QDialogButtonBox();
-	QPushButton::connect(buttons->addButton(QDialogButtonBox::Ok),
-		&QPushButton::clicked, [this](bool checked) {
-		accept();
-	});
+	QPushButton::connect(
+		buttons->addButton(QDialogButtonBox::Ok), &QPushButton::clicked,
+		[this](bool checked)
+		{
+			accept();
+		});
 	layout->addWidget(buttons);
 }
 
