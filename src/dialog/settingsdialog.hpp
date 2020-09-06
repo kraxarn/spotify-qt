92d1c0540537a8d45563568c22c945adc7fc34c8
kraxarn <me@kraxarn.com>
2020-09-06 14:03:51 +0200

    Revert "Move SpotifySettings to custom class"
    This reverts commit 4783fab0882e39b15d0a36b89dab4738f619a8e2.

--- src/dialog/settingsdialog.hpp
+++ src/dialog/settingsdialog.hpp
@@ -40,8 +40,22 @@ public:
 	explicit SettingsDialog(Settings &settings, QWidget *parent = nullptr);
 
 private:
+	QWidget *spotifySettings();
+	QWidget *aboutSettings();
 	bool applySettings();
+	static bool sptConfigExists();
+	void globalConfigToggle(int state);
 
 	Settings &settings;
 	QVector<SettingsPage*> pages;
+
+	// Spotify settings
+	QCheckBox *sptAlways = nullptr;
+	QCheckBox *sptAppStart = nullptr;
+	QCheckBox *sptGlobal = nullptr;
+	QComboBox *sptBitrate = nullptr;
+	QGroupBox *sptGroup = nullptr;
+	QLabel *sptVersion = nullptr;
+	QLineEdit *sptPath = nullptr;
+	QLineEdit *sptUsername = nullptr;
 };
--- src/settingspage/pages.hpp
+++ src/settingspage/pages.hpp
@@ -3,4 +3,3 @@
 #include "appsettings.hpp"
 #include "interfacesettings.hpp"
 #include "traysettings.hpp"
-#include "spotifysettings.hpp"
