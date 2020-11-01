debb911edeb3bf4906ebb2c03338b88b51c09501
kraxarn <me@kraxarn.com>
2020-11-01 17:52:00 +0100

    Log with templates

--- src/log.hpp
+++ src/log.hpp
@@ -11,9 +11,28 @@
 class Log
 {
 public:
+	template<typename... Args>
+	static void info(const QString &message, Args&... args)
+	{
+		info(collect(message, args...));
+	}
 	static void info(const QString &message);
+
+	template<typename ... Args>
+	static void warn(const QString &message, Args&... args)
+	{
+		warn(collect(message, args...));
+	}
 	static void warn(const QString &message);
+
+	template<typename ... Args>
+	static void error(const QString &message, Args&... args)
+	{
+		error(collect(message, args...));
+	}
 	static void error(const QString &message);
+
+	static const QList<LogMessage> &getMessages();
 
 private:
 	Log() = default;
@@ -21,4 +40,10 @@ private:
 	static QList<LogMessage> messages;
 
 	static void log(LogType logType, const QString &message);
+
+	template<typename T>
+	static QString collect(const QString &message, T &arg)
+	{
+		return QString(message).arg(arg);
+	}
 };
