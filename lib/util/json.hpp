7c3b23156191ef62188d8076cebdd013dd70ed9b
kraxarn <me@kraxarn.com>
2020-11-29 19:19:40 +0100

    JSON helper to get or fallback

--- lib/util/json.hpp
+++ lib/util/json.hpp
@@ -17,5 +17,21 @@ namespace lib
 		 * @param value Value to write to if found
 		 */
 		static nlohmann::json get_property(const nlohmann::json &json, const std::initializer_list<std::string> &keys);
+
+		/**
+		 * Get value from json, or a default value if not found
+		 * @tparam T Value type
+		 * @param json JSON to get value from
+		 * @param key Key to find
+		 * @param fallback Default value
+		 * @return Value if found, or default
+		 */
+		template<typename T>
+		static T get_or_default(const nlohmann::json &json, const std::string &key, const T &fallback)
+		{
+			return json.contains(key)
+				? json.at(key).get<T>()
+				: fallback;
+		}
 	};
 }
