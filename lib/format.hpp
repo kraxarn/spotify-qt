e5a68670f9103324ff7501005cdb9b9243f33e59
kraxarn <me@kraxarn.com>
2020-11-29 11:10:48 +0100

    Documentation

--- lib/format.hpp
+++ lib/format.hpp
@@ -11,6 +11,38 @@ namespace lib
 	 */
 	class fmt
 	{
+	public:
+		/**
+		 * Format a string as a string
+		 * @param fmt string
+		 * @return string
+		 */
+		template<typename Format>
+		static std::string format(const Format &fmt)
+		{
+			return fmt;
+		}
+
+		/**
+		 * Format a string with specified arguments as {} in the string
+		 * @param fmt string
+		 * @param arg Argument to format
+		 * @param args Arguments to format
+		 * @return Formatted string
+		 */
+		template<typename Format, typename Arg, typename... Args>
+		static std::string format(const Format &fmt, const Arg &arg, Args &&... args)
+		{
+			return format(collect(fmt, arg), args...);
+		}
+
+	private:
+		/**
+		 * Format string into another string
+		 * @param fmt String with {} to replace
+		 * @param arg String to replace {} with
+		 * @return Formatted string
+		 */
 		template<typename Format>
 		static std::string collect(const Format &fmt, const std::string &arg)
 		{
@@ -22,34 +54,37 @@ namespace lib
 			return str.replace(index, 2, arg);
 		}
 
+		/**
+		 * Format value into a string
+		 * @param fmt String with {} to replace
+		 * @param arg Value to replace {} with
+		 * @return Formatted string
+		 */
 		template<typename Format, typename Arg>
 		static std::string collect(const Format &fmt, const Arg &arg)
 		{
 			return collect(fmt, std::to_string(arg));
 		}
 
+		/**
+		 * Format C-style string into a string
+		 * @param fmt String with {} to replace
+		 * @param arg C-style string to replace {} with
+		 * @return Formatted string
+		 */
 		template<typename Format>
 		static std::string collect(const Format &fmt, const char *arg)
 		{
 			return collect(fmt, std::string(arg));
 		}
 
+		/**
+		 * Deprecated
+		 */
 		template<typename Format>
 		static std::string collect(const Format &fmt, char *arg)
 		{
 			return collect(fmt, std::string(arg));
 		}
-
-		template<typename Format>
-		static std::string format(const Format &fmt)
-		{
-			return fmt;
-		}
-
-		template<typename Format, typename Arg, typename... Args>
-		static std::string format(const Format &fmt, const Arg &arg, Args &&... args)
-		{
-			return format(collect(fmt, arg), args...);
-		}
-	}
+	};
 }
