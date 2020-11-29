a043f5e547c53d167da752b2e6c6aab95f1d439d
kraxarn <me@kraxarn.com>
2020-11-29 19:18:14 +0100

    Move date format to custom date class

---
+++ lib/util/date.cpp
@@ -0,0 +1,73 @@
+#include "date.hpp"
+
+using namespace lib;
+
+#define LOCAL_TIME_FORMAT "%X"
+#define LOCALE_DATE_FORMAT "%x"
+#define ISO_DATE_FORMAT "%Y-%m-%d"
+#define ISO_DATE_TIME_FORMAT "%Y-%m-%dT%H:%M:%SZ"
+
+date date::parse(const std::string &value)
+{
+	lib::date date;
+
+	// First try to parse as full date and time
+	date.parse(value, ISO_DATE_TIME_FORMAT);
+
+	// Then try to pars as date only
+	if (!date.is_valid())
+		date.parse(value, ISO_DATE_FORMAT);
+
+	// If it's still invalid, something is wrong
+	if (!date.is_valid())
+		log::warn("Failed to parse \"{}\" as a date")
+}
+
+bool date::is_valid() const
+{
+	return tm != nullptr;
+}
+
+void date::parse(const std::string &value, const char *format)
+{
+	std::stringstream(value) >> std::get_time(tm, format);
+}
+
+std::string date::to_time() const
+{
+	return format(LOCAL_TIME_FORMAT);
+}
+
+std::string date::to_date() const
+{
+	return format(LOCALE_DATE_FORMAT);
+}
+
+std::string date::to_iso_date() const
+{
+	return format(ISO_DATE_FORMAT);
+}
+
+
+std::string date::to_iso_date_time() const
+{
+	return format(ISO_DATE_TIME_FORMAT);
+}
+
+std::string date::format(const char *format) const
+{
+	if (tm == nullptr)
+		return std::string();
+
+	char buffer[64];
+	std::strftime(buffer, sizeof(buffer), format, tm);
+	return std::string(buffer);
+}
+
+bool date::is_empty() const
+{
+	return tm == nullptr
+		|| (tm->tm_year <= 70
+			&& tm->tm_mon == 0
+			&& tm->tm_mday == 0);
+}
---
+++ lib/util/date.hpp
@@ -0,0 +1,85 @@
+#pragma once
+
+#include "log.hpp"
+
+#include <string>
+#include <iomanip>
+#include <sstream>
+
+namespace lib
+{
+	/**
+	 * Date and time
+	 */
+	class date
+	{
+	public:
+		/**
+		 * Construct a new empty and invalid date
+		 */
+		date() = default;
+
+		/**
+		 * Try to parse a date from a string
+		 * @param value ISO date
+		 * @return A date, invalid if parsing failed
+		 */
+		static date parse(const std::string &value);
+
+		/**
+		 * If the current instance represents a valid date
+		 * @return Date is valid
+		 */
+		bool is_valid() const;
+
+		/**
+		 * Date is 1970-01-01 or invalid
+		 * @return Date is empty
+		 */
+		bool is_empty() const;
+
+		/**
+		 * Get locale dependent time
+		 * @return Time
+		 */
+		std::string to_time() const;
+
+		/**
+		 * Get locale dependent date
+		 * @return Date
+		 */
+		std::string to_date() const;
+
+		/**
+		 * Get ISO date (YYYY-MM-DD)
+		 * @return Date
+		 */
+		std::string to_iso_date() const;
+
+		/**
+		 * Get ISO date and time (YYYY-MM-DDTHH:MM:SSZ)
+		 * @return Date and time
+		 */
+		std::string to_iso_date_time() const;
+
+	private:
+		/**
+		 * Parse a string as a date using the specified format
+		 * @param value Date to parse
+		 * @param format Expected format
+		 */
+		void parse(const std::string &value, const char* format);
+
+		/**
+		 * Format a date as a string using the specified format
+		 * @param format Format to use
+		 * @return Formatted date as string, or an empty string
+		 */
+		std::string format(const char* format) const;
+
+		/**
+		 * Internal date structure
+		 */
+		std::tm *tm = nullptr;
+	};
+}
