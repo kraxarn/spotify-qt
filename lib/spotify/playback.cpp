a9315965b34ec361e1913a8ec754d9101a5e4efe
kraxarn <me@kraxarn.com>
2020-11-29 12:28:43 +0100

    Moved playback

---
+++ lib/spotify/playback.cpp
@@ -0,0 +1,59 @@
+#include "playback.hpp"
+
+using namespace lib::spt;
+
+Playback::Playback(const nlohmann::json &json)
+{
+	if (json.at("item").is_null())
+		return;
+
+	json.at("progress_ms").get_to(progressMs);
+	item = Track(json.at("item"));
+	json.at("is_playing").get_to(isPlaying);
+	json.at("repeat_state").get_to(repeat);
+	json.at("shuffle_state").get_to(shuffle);
+	if (json.at("context").is_object())
+	{
+		auto context = json.at("context");
+		context.at("uri").get_to(contextUri);
+		context.at("type").get_to(contextType);
+	}
+	device = Device(json.at("device"));
+}
+
+nlohmann::json Playback::metadata() const
+{
+	std::string itemName(isPlaying ? item.name : "");
+	std::vector<std::string> itemArtist(isPlaying ? item.artist : "");
+	std::string itemAlbum(isPlaying ? item.album : "");
+	std::string itemId(isPlaying ? item.id : "");
+	auto itemDuration = isPlaying ? item.duration : 0;
+	std::string itemImage(isPlaying ? item.image : "");
+
+	return {
+		{"xesam:title", itemName},
+		{"xesam:artist", itemArtist},
+		{"xesam:album", itemAlbum},
+		{"xesam:albumArtist", itemArtist},
+		{"xesam:url", fmt::format("https://open.spotify.com/track/{}", itemId)},
+		{"mpris:length", itemDuration * 1000},
+		{"mpris:artUrl", itemImage},
+		{"mpris:trackid", fmt::format("spotify:track:{}", itemId)},
+	};
+}
+
+nlohmann::json Playback::toJson() const
+{
+	return {
+		{"progress_ms", progressMs},
+		{"item", item.toJson()},
+		{"is_playing", isPlaying},
+		{"repeat", repeat},
+		{"shuffle", shuffle}
+	};
+}
+
+int Playback::volume() const
+{
+	return device.volumePercent;
+}
---
+++ lib/spotify/playback.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "device.hpp"
+#include "format.hpp"
+#include "track.hpp"
+
+#include "lib/json.hpp"
+
+namespace lib
+{
+	namespace spt
+	{
+		/**
+		 * Currently playing
+		 */
+		class Playback
+		{
+		public:
+			Playback() = default;
+			explicit Playback(const nlohmann::json &json);
+
+			nlohmann::json metadata() const;
+			nlohmann::json toJson() const;
+
+			int volume() const;
+
+			std::string contextType;
+			std::string contextUri;
+			std::string repeat = "off";
+			Track item = Track();
+			bool isPlaying = false;
+			bool shuffle = false;
+			int progressMs = 0;
+			Device device;
+		};
+	}
+}
