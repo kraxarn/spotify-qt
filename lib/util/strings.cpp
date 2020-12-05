2bc8e50243f391383da33ee2f342e7a7d296b198
kraxarn <me@kraxarn.com>
2020-12-05 11:17:34 +0100

    More string utilities

--- lib/util/strings.cpp
+++ lib/util/strings.cpp
@@ -7,7 +7,48 @@ std::string strings::join(const std::vector<std::string> &strings, const char *s
 
 	return std::accumulate(std::next(strings.begin()), strings.end(), strings[0],
 		[separator](const std::string &str1, const std::string &str2)
+		{
+			return str1 + separator + str2;
+		});
+}
+
+std::string strings::trim(std::string &str)
+{
+	trim_begin(str);
+	trim_end(str);
+
+	return str;
+}
+
+void strings::trim_begin(std::string &str)
+{
+	str.erase(str.begin(), std::find_if(str.begin(), str.end(), [](unsigned char chr)
 	{
-		return str1 + separator + str2;
-	});
+		return !std::isspace(chr);
+	}));
+}
+
+void strings::trim_end(std::string &str)
+{
+	str.erase(std::find_if(str.rbegin(), str.rend(), [](unsigned char chr)
+	{
+		return !std::isspace(chr);
+	}).base(), str.end());
+}
+
+std::vector<std::string> strings::split(const std::string &str, char delimiter)
+{
+	std::vector<std::string> vec;
+	std::istringstream stream(str);
+	std::string temp;
+
+	while (std::getline(stream, temp, delimiter))
+		vec.push_back(temp);
+
+	return vec;
+}
+
+bool strings::starts_with(const std::string &str, const std::string &start)
+{
+	return str.find(start, 0) == 0;
 }
--- lib/util/strings.hpp
+++ lib/util/strings.hpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <vector>
 #include <numeric>
+#include <algorithm>
 
 /**
  * String utilities
@@ -31,4 +32,41 @@ public:
 	{
 		return str.find(value) != std::string::npos;
 	}
+
+	/**
+	 * Trim start and end of any whitespace
+	 * @param str String to trim
+	 * @remark Argument is modified, return value is the same as the argument
+	 * @return Trimmed string
+	 */
+	static std::string trim(std::string &str);
+
+	/**
+	 * Split a string into a vector
+	 * @param str String to split
+	 * @param delimiter Delimiter to split by
+	 * @return Split string
+	 */
+	static std::vector<std::string> split(const std::string &str, char delimiter);
+
+	/**
+	 * Check if a string starts with a specific string
+	 * @param str String to check
+	 * @param start String that the string should start with
+	 * @return String starts with the specified string
+	 */
+	static bool starts_with(const std::string &str, const std::string &start);
+
+private:
+	/**
+	 * Trim beginning of string
+	 * @param str String to trim
+	 */
+	static void trim_begin(std::string &str);
+
+	/**
+	 * Trim end of string
+	 * @param str String to trim
+	 */
+	static void trim_end(std::string &str);
 };
