38de27e168d7966a0cbc92b6276a3f0047228beb
kraxarn <me@kraxarn.com>
2020-12-12 16:47:12 +0100

    Fix namespaces

--- lib/spotify/core/spotify.cpp
+++ lib/spotify/core/spotify.cpp
@@ -1,11 +1,11 @@
 #include "spotify.hpp"
 
-lib::spt::core::spotify::spotify(lib::Settings &settings)
+lib::spt::spotify::spotify(lib::Settings &settings)
 	: api(settings)
 {
 }
 
-nlohmann::json lib::spt::core::spotify::get(const std::string &url)
+nlohmann::json lib::spt::spotify::get(const std::string &url)
 {
 	return api.get(url);
 }
