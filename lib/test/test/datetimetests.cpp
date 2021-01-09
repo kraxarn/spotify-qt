636e198735c9beb75fae03934e11985c5f83e883
kraxarn <me@kraxarn.com>
2021-01-09 18:20:27 +0100

    Use multiple TEST_CASE instead

--- lib/test/test/datetimetests.cpp
+++ lib/test/test/datetimetests.cpp
@@ -1,24 +1,11 @@
 #include "lib/datetime.hpp"
 #include "thirdparty/doctest.h"
 
-TEST_CASE("date_time tests")
+TEST_CASE("date_time::now(_utc)")
 {
 	lib::date_time date_time;
 
-	auto validate_date = [&date_time](int year, int month, int day,
-		int hour = 0, int minute = 0, int second = 0)
-	{
-		CHECK(date_time.is_valid());
-
-		CHECK_EQ(year, date_time.get_year());
-		CHECK_EQ(month, date_time.get_month());
-		CHECK_EQ(day, date_time.get_day());
-		CHECK_EQ(hour, date_time.get_hour());
-		CHECK_EQ(minute, date_time.get_minute());
-		CHECK_EQ(second, date_time.get_second());
-	};
-
-	SUBCASE("can get now")
+	SUBCASE("now")
 	{
 		date_time = lib::date_time::now();
 
@@ -29,7 +16,7 @@ TEST_CASE("date_time tests")
 		CHECK(date_time.get_day() >= 1);
 	}
 
-	SUBCASE("can get now utc")
+	SUBCASE("now utc")
 	{
 		date_time = lib::date_time::now_utc();
 
@@ -39,14 +26,32 @@ TEST_CASE("date_time tests")
 		CHECK(date_time.get_month() >= 1);
 		CHECK(date_time.get_day() >= 1);
 	}
+}
 
-	SUBCASE("can parse iso date time")
+TEST_CASE("date_time::parse")
+{
+	lib::date_time date_time;
+
+	auto validate_date = [&date_time](int year, int month, int day,
+		int hour = 0, int minute = 0, int second = 0)
+	{
+			CHECK(date_time.is_valid());
+
+			CHECK_EQ(year, date_time.get_year());
+			CHECK_EQ(month, date_time.get_month());
+			CHECK_EQ(day, date_time.get_day());
+			CHECK_EQ(hour, date_time.get_hour());
+			CHECK_EQ(minute, date_time.get_minute());
+			CHECK_EQ(second, date_time.get_second());
+	};
+
+	SUBCASE("iso date time")
 	{
 		date_time = lib::date_time::parse("2001-02-03T14:15:16Z");
 		validate_date(2001, 02, 03, 14, 15, 16);
 	}
 
-	SUBCASE("can parse iso date")
+	SUBCASE("iso date")
 	{
 		date_time = lib::date_time::parse("2002-03-04");
 		validate_date(2002, 03, 04);
