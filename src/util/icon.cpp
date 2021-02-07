a2726228ff646f0c62d5484c64aa02aa9fd6bd05
kraxarn <me@kraxarn.com>
2021-02-07 22:48:06 +0100

    Formatting

--- src/util/icon.cpp
+++ src/util/icon.cpp
@@ -5,11 +5,15 @@ bool Icon::useFallbackIcons = false;
 QIcon Icon::get(const QString &name)
 {
 	if (name.startsWith("logo:"))
+	{
 		return QIcon(QString(":/res/logo/%1.svg")
 			.arg(name.right(name.length() - QString("logo:").length())));
+	}
+
 	auto fallback = QString(":/res/ic/%1/%2.svg")
 		.arg(Utils::darkBackground ? "dark" : "light")
 		.arg(name);
+
 	return useFallbackIcons
 		? QIcon(fallback)
 		: QIcon::fromTheme(name, QIcon(fallback));
