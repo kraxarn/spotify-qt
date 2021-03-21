1aa3fbd230c21f6225ac8cbf3fae074b9006cc8e
kraxarn <me@kraxarn.com>
2021-03-21 19:18:35 +0100

    Updated cache for loading/saving tracks

--- lib/include/lib/cache.hpp
+++ lib/include/lib/cache.hpp
@@ -2,9 +2,11 @@
 
 #include "thirdparty/filesystem.hpp"
 #include "thirdparty/json.hpp"
-#include "paths/paths.hpp"
-#include "format.hpp"
-#include "log.hpp"
+#include "lib/paths/paths.hpp"
+#include "lib/format.hpp"
+#include "lib/log.hpp"
+#include "lib/json.hpp"
+#include "lib/spotify/track.hpp"
 
 namespace lib
 {
@@ -20,22 +22,64 @@ namespace lib
 		 */
 		explicit cache(const paths &paths);
 
+		//region album
+
+		//endregion
+
+		//region playlist
+
+		/**
+		 * Loads tracks from a cached playlist
+		 * @param id Playlist ID
+		 * @return Tracks or an empty vector on failure
+		 */
+		std::vector<lib::spt::track> get_playlist_tracks(const std::string &id);
+
+		void set_playlist_tracks(const std::string &id,
+			const std::vector<lib::spt::track> &tracks);
+
+		//endregion
+
+		//region tracks
+
 		/**
 		 * Get tracks saved in cache
-		 * @note Will soon return std::vector<spt::track>
 		 * @param id Id of album for example
 		 * @return JSON stored in cache, or an empty object if none
 		 */
-		nlohmann::json tracks(const std::string &id);
+		std::vector<lib::spt::track> tracks(const std::string &id);
+
+		/**
+		 * Save tracks to cache
+		 * @param id Id of album for example
+		 * @param tracks Tracks to save
+		 */
+		void tracks(const std::string &id, const std::vector<lib::spt::track> &tracks);
 
 		/**
 		 * Get all tracks saved in cache
-		 * @note Will soon return std::map<std::string, std::vector<spt::track>>
 		 * @return Map as id: tracks
 		 */
-		std::map<std::string, nlohmann::json> all_tracks();
+		std::map<std::string, std::vector<lib::spt::track>> all_tracks();
+
+		//endregion
 
 	private:
 		const lib::paths &paths;
+
+		/**
+		 * Get parent directory for cache type
+		 */
+		ghc::filesystem::path dir(const std::string &type);
+
+		/**
+		 * Get file name for id
+		 */
+		static std::string file(const std::string &id);
+
+		/**
+		 * Get full file path for cache type and id
+		 */
+		ghc::filesystem::path path(const std::string &type, const std::string &id);
 	};
 }
--- lib/src/cache.cpp
+++ lib/src/cache.cpp
@@ -5,26 +5,67 @@ lib::cache::cache(const lib::paths &paths)
 {
 }
 
-nlohmann::json lib::cache::tracks(const std::string &id)
+ghc::filesystem::path lib::cache::dir(const std::string &type)
 {
-	auto file_path = ghc::filesystem::path(paths.cache())
-		/ "tracks"
-		/ fmt::format("{}.json", id);
+	auto file_dir = ghc::filesystem::path(paths.cache()) / type;
 
-	if (!ghc::filesystem::exists(file_path))
-		return nlohmann::json::object();
+	if (!ghc::filesystem::exists(file_dir))
+		ghc::filesystem::create_directories(file_dir);
 
-	std::ifstream file(file_path);
-	nlohmann::json json;
-	file >> json;
-	file.close();
-	return json;
+	return file_dir;
 }
 
-std::map<std::string, nlohmann::json> lib::cache::all_tracks()
+std::string lib::cache::file(const std::string &id)
+{
+	return fmt::format("{}.json", id);
+}
+
+ghc::filesystem::path lib::cache::path(const std::string &type, const std::string &id)
+{
+	return dir(type) / file(id);
+}
+
+//region album
+
+//endregion
+
+//region playlist
+
+std::vector<lib::spt::track> lib::cache::get_playlist_tracks(const std::string &id)
+{
+	return lib::json::load_items<lib::spt::track>(path("playlist", id));
+}
+
+void lib::cache::set_playlist_tracks(const std::string &id,
+	const std::vector<lib::spt::track> &tracks)
+{
+	lib::json::save_items(path("playlist", id), tracks);
+}
+
+//endregion
+
+//region tracks
+
+std::vector<lib::spt::track> lib::cache::tracks(const std::string &id)
+{
+	auto file_path = path("tracks", id);
+
+	if (!ghc::filesystem::exists(file_path))
+		return std::vector<lib::spt::track>();
+
+	return lib::json::load_items<lib::spt::track>(file_path);
+}
+
+void lib::cache::tracks(const std::string &id, const std::vector<lib::spt::track> &tracks)
+{
+	auto file_path = path("tracks", id);
+	lib::json::save_items(file_path, tracks);
+}
+
+std::map<std::string, std::vector<lib::spt::track>> lib::cache::all_tracks()
 {
 	auto dir = ghc::filesystem::path(paths.cache()) / "tracks";
-	std::map<std::string, nlohmann::json> results;
+	std::map<std::string, std::vector<lib::spt::track>> results;
 
 	if (!ghc::filesystem::exists(dir))
 		return results;
@@ -37,3 +78,5 @@ std::map<std::string, nlohmann::json> lib::cache::all_tracks()
 
 	return results;
 }
+
+//endregion
