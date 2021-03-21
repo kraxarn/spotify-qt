5fc89e8450cd6424ab9e3a6ee68ec3ba9704d789
kraxarn <me@kraxarn.com>
2021-03-21 19:34:12 +0100

    New temporary player class

---
+++ lib/src/spotifyapi/player.cpp
@@ -0,0 +1,31 @@
+#include "lib/spotify/spotifyapi.hpp"
+
+using namespace lib::spt;
+
+// current_playback
+
+// set_device
+
+// devices
+
+// play_tracks
+
+// resume
+
+// pause
+
+// next
+
+// previous
+
+// seek
+
+// set_repeat
+
+// set_volume
+
+// set_shuffle
+
+// recently_played
+
+// add_to_queue
