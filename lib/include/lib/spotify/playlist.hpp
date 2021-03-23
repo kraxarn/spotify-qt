14e1a7382d3087b1e62e480c16a2a086ea98203c
kraxarn <me@kraxarn.com>
2021-03-23 20:28:01 +0100

    New playlist class (without paging for now)

---
+++ lib/include/lib/spotify/playlist.hpp
@@ -0,0 +1,86 @@
+#pragma once
+
+#include "lib/spotify/user.hpp"
+#include "lib/spotify/track.hpp"
+#include "lib/json.hpp"
+
+#include <string>
+
+namespace lib
+{
+	namespace spt
+	{
+		/**
+		 * Playlist
+		 */
+		class playlist
+		{
+		public:
+			playlist() = default;
+
+			/**
+			 * Optional playlist description
+			 */
+			std::string description;
+
+			/**
+			 * Playlist ID
+			 */
+			std::string id;
+
+			/**
+			 * URL to playlist image
+			 */
+			std::string image;
+
+			/**
+			 * Name of playlist
+			 */
+			std::string name;
+
+			/**
+			 * Snapshot ID
+			 */
+			std::string snapshot;
+
+			/**
+			 * Spotify ID of owner
+			 */
+			std::string owner_id;
+
+			/**
+			 * Display name of owner
+			 */
+			std::string owner_name;
+
+			/**
+			 * Collaborative playlist, where anyone can edit it
+			 */
+			bool collaborative = false;
+
+			/**
+			 * Readable and searchable by the public
+			 */
+			bool is_public = false;
+
+			/**
+			 * Tracks in playlist
+			 */
+			std::vector<lib::spt::track> tracks;
+
+			/**
+			 * Specified user is the owner of this playlist
+			 */
+			bool is_owner(const lib::spt::user &user) const;
+
+			/**
+			 * If no playlist has been parsed (invalid ID)
+			 */
+			bool is_null() const;
+		};
+
+		void to_json(nlohmann::json &j, const playlist &p);
+
+		void from_json(const nlohmann::json &j, playlist &p);
+	}
+}
---
+++ lib/src/spotify/playlist.cpp
@@ -0,0 +1,66 @@
+#include "lib/spotify/playlist.hpp"
+
+void lib::spt::to_json(nlohmann::json &j, const playlist &p)
+{
+	j = nlohmann::json{
+		{"collaborative", p.collaborative},
+		{"description", p.description},
+		{"id", p.id},
+		{"image", p.image},
+		{"name", p.name},
+		{"is_public", p.is_public},
+		{"total", p.tracks.size()},
+		{"tracks", p.tracks},
+		{"snapshot", p.snapshot},
+		{"owner_id", p.owner_id},
+		{"owner_name", p.owner_name}
+	};
+}
+
+void lib::spt::from_json(const nlohmann::json &j, playlist &p)
+{
+	if (!j.is_object())
+		return;
+
+	j.at("collaborative").get_to(p.collaborative);
+	j.at("description").get_to(p.description);
+	j.at("id").get_to(p.id);
+	j.at("name").get_to(p.name);
+	lib::json::get(j, "is_public", p.is_public);
+	lib::json::get_property(j, {
+		"is_public", "public"
+	}, p.is_public);
+	j.at("tracks").get_to(p.tracks);
+	lib::json::get_property(j, {
+		"snapshot", "snapshot_id"
+	}, p.snapshot);
+
+	if (j.contains("image"))
+		j.at("image").get_to(p.image);
+	else
+		j.at("images").front().at("url").get_to(p.image);
+
+	if (j.contains("owner_id"))
+		j.at("owner_id").get_to(p.owner_id);
+	else if (j.contains("ownerId"))
+		j.at("ownerId").get_to(p.owner_id);
+	else
+		j.at("owner").at("id").get_to(p.owner_id);
+
+	if (j.contains("owner_name"))
+		j.at("owner_name").get_to(p.owner_name);
+	else if (j.contains("ownerName"))
+		j.at("ownerName").get_to(p.owner_name);
+	else
+		j.at("owner").at("display_name").get_to(p.owner_name);
+}
+
+bool lib::spt::playlist::is_owner(const lib::spt::user &user) const
+{
+	return !owner_id.empty() && owner_id == user.id;
+}
+
+bool lib::spt::playlist::is_null() const
+{
+	return id.empty();
+}
