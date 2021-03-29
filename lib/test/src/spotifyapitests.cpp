83a65657d03e501dd968c2a4ac6dd01138290d30
kraxarn <me@kraxarn.com>
2021-03-29 20:49:14 +0200

    New unit tests to to_uri/to_id

---
+++ lib/test/src/spotifyapitests.cpp
@@ -0,0 +1,24 @@
+#include "thirdparty/doctest.h"
+#include "lib/spotify/spotifyapi.hpp"
+
+TEST_CASE("spotify_api")
+{
+	SUBCASE("to_uri")
+	{
+		CHECK_EQ(lib::spt::spotify_api::to_uri("track", "4uLU6hMCjMI75M1A2tKUQC"),
+			"spotify:track:4uLU6hMCjMI75M1A2tKUQC");
+
+		CHECK_EQ(lib::spt::spotify_api::to_uri("track",
+			"spotify:track:4uLU6hMCjMI75M1A2tKUQC"),
+			"spotify:track:4uLU6hMCjMI75M1A2tKUQC");
+	}
+
+	SUBCASE("to_id")
+	{
+		CHECK_EQ(lib::spt::spotify_api::to_id("spotify:track:4uLU6hMCjMI75M1A2tKUQC"),
+			"4uLU6hMCjMI75M1A2tKUQC");
+
+		CHECK_EQ(lib::spt::spotify_api::to_id("4uLU6hMCjMI75M1A2tKUQC"),
+			"4uLU6hMCjMI75M1A2tKUQC");
+	}
+}
