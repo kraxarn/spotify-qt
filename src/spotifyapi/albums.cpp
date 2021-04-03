e4ce66b176f28d200a9d4aca02d61f0a408d2cec
kraxarn <me@kraxarn.com>
2021-04-03 12:07:09 +0200

    Fix album track load

--- src/spotifyapi/albums.cpp
+++ src/spotifyapi/albums.cpp
@@ -8,17 +8,17 @@ void Spotify::getAlbum(const std::string &id, lib::callback<lib::spt::album> &ca
 void Spotify::albumTracks(const lib::spt::album &album,
 	lib::callback<std::vector<lib::spt::track>> &callback)
 {
-	get(lib::fmt::format("albums/{}/tracks?limit=50",
-		album.id), [album, callback](const std::vector<lib::spt::track> &results)
-	{
-		std::vector<lib::spt::track> tracks;
-		tracks.reserve(results.size());
-		for (auto &result : results)
+	lib::spt::spotify_api::get(lib::fmt::format("albums/{}/tracks?limit=50", album.id),
+		"items", [album, callback](const std::vector<lib::spt::track> &results)
 		{
-			lib::spt::track track = result;
-			track.album = album.name;
-			tracks.push_back(track);
-		}
-		callback(tracks);
-	});
+			std::vector<lib::spt::track> tracks;
+			tracks.reserve(results.size());
+			for (auto &result : results)
+			{
+				lib::spt::track track = result;
+				track.album = album.name;
+				tracks.push_back(track);
+			}
+			callback(tracks);
+		});
 }
