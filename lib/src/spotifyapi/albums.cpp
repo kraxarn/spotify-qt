f838aa0d505af157e8f05679c7ac50f5c5d33a8e
kraxarn <me@kraxarn.com>
2021-04-04 15:40:28 +0200

    Albums API moved to lib

--- lib/src/spotifyapi/albums.cpp
+++ lib/src/spotifyapi/albums.cpp
@@ -1,11 +1,13 @@
-#include "spotifyapi.hpp"
+#include "lib/spotify/spotifyapi.hpp"
 
-void Spotify::getAlbum(const std::string &id, lib::callback<lib::spt::album> &callback)
+using namespace lib::spt;
+
+void spotify_api::album(const std::string &id, lib::callback<lib::spt::album> &callback)
 {
 	get(lib::fmt::format("albums/{}", id), callback);
 }
 
-void Spotify::albumTracks(const lib::spt::album &album,
+void spotify_api::album_tracks(const lib::spt::album &album,
 	lib::callback<std::vector<lib::spt::track>> &callback)
 {
 	lib::spt::spotify_api::get(lib::fmt::format("albums/{}/tracks?limit=50", album.id),
