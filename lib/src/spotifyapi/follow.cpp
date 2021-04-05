7252484029dd5cc779e025cba1b0b8eb11dd6c9c
kraxarn <me@kraxarn.com>
2021-04-05 17:13:31 +0200

    Fix followed artists

--- lib/src/spotifyapi/follow.cpp
+++ lib/src/spotifyapi/follow.cpp
@@ -8,7 +8,7 @@ using namespace lib::spt;
 
 void api::followed_artists(lib::callback<std::vector<lib::spt::artist>> &callback)
 {
-	get_items("me/following?type=artist&limit=50", callback);
+	get_items("me/following?type=artist&limit=50", "artists", callback);
 }
 
 void api::follow(lib::follow_type type, const std::vector<std::string> &ids,
