144e17a18cf3351570d9ae470fc22d785dac2b29
kraxarn <me@kraxarn.com>
2021-04-05 16:52:50 +0200

    Fix tests

--- lib/test/src/spotifyapitests.cpp
+++ lib/test/src/spotifyapitests.cpp
@@ -5,20 +5,20 @@ TEST_CASE("spotify_api")
 {
 	SUBCASE("to_uri")
 	{
-		CHECK_EQ(lib::spt::spotify_api::to_uri("track", "4uLU6hMCjMI75M1A2tKUQC"),
+		CHECK_EQ(lib::spt::api::to_uri("track", "4uLU6hMCjMI75M1A2tKUQC"),
 			"spotify:track:4uLU6hMCjMI75M1A2tKUQC");
 
-		CHECK_EQ(lib::spt::spotify_api::to_uri("track",
+		CHECK_EQ(lib::spt::api::to_uri("track",
 			"spotify:track:4uLU6hMCjMI75M1A2tKUQC"),
 			"spotify:track:4uLU6hMCjMI75M1A2tKUQC");
 	}
 
 	SUBCASE("to_id")
 	{
-		CHECK_EQ(lib::spt::spotify_api::to_id("spotify:track:4uLU6hMCjMI75M1A2tKUQC"),
+		CHECK_EQ(lib::spt::api::to_id("spotify:track:4uLU6hMCjMI75M1A2tKUQC"),
 			"4uLU6hMCjMI75M1A2tKUQC");
 
-		CHECK_EQ(lib::spt::spotify_api::to_id("4uLU6hMCjMI75M1A2tKUQC"),
+		CHECK_EQ(lib::spt::api::to_id("4uLU6hMCjMI75M1A2tKUQC"),
 			"4uLU6hMCjMI75M1A2tKUQC");
 	}
 }
