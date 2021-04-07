f934f1d11da47f29cf329aaad1328297afc13ff4
kraxarn <kraxarn@users.noreply.github.com>
2021-04-07 20:53:52 +0000

    deploy: 868ac3cb1928ae8a8aad39f7094ffcb859505131

--- search/all_3.js
+++ search/all_3.js
@@ -1,14 +1,14 @@
 var searchData=
 [
-  ['date_5ftime_27',['date_time',['../classlib_1_1date__time.html#a9a9097c65dacf644b2f6fcc12b61bd80',1,'lib::date_time::date_time()=default'],['../classlib_1_1date__time.html#a2321721ead30e1e788de467afbe9987c',1,'lib::date_time::date_time(int year, int month, int day, int hour, int minute, int second)'],['../classlib_1_1date__time.html#a5efb61cb61a126c74f122dc91fab60b3',1,'lib::date_time::date_time(const date_time &amp;date)'],['../classlib_1_1date__time.html',1,'lib::date_time']]],
-  ['decode_28',['decode',['../classlib_1_1base64.html#ae42ec5dcbc7448282b4f64909e95f31a',1,'lib::base64']]],
-  ['del_29',['del',['../classlib_1_1spt_1_1api.html#ae1c1d6b6d175f6897eee141d502ca313',1,'lib::spt::api::del(const std::string &amp;url, lib::callback&lt; std::string &gt; &amp;callback)'],['../classlib_1_1spt_1_1api.html#a71a727da67641b5a574086b05e1f1afb',1,'lib::spt::api::del(const std::string &amp;url, const nlohmann::json &amp;json, lib::callback&lt; std::string &gt; &amp;callback)=0']]],
-  ['deprecated_20list_30',['Deprecated List',['../deprecated.html',1,'']]],
-  ['description_31',['description',['../classlib_1_1spt_1_1playlist.html#af058c4eb3226f7b4d8cd270acdfa2514',1,'lib::spt::playlist']]],
-  ['dev_32',['dev',['../classlib_1_1log.html#a6faeebb4fdad6162e122a5941e8ae323',1,'lib::log::dev(const Format &amp;fmt, const Arg &amp;arg, Args &amp;&amp;... args)'],['../classlib_1_1log.html#af971c261aeceb74c0e806a9ea3e5867e',1,'lib::log::dev(const Format &amp;fmt)']]],
-  ['developer_5fmode_33',['developer_mode',['../classlib_1_1developer__mode.html',1,'lib']]],
-  ['device_34',['device',['../classlib_1_1spt_1_1playback.html#a347a1f37e20750a9ffaaf42cf93780bd',1,'lib::spt::playback::device()'],['../classlib_1_1spt_1_1device.html',1,'lib::spt::device']]],
-  ['devices_35',['devices',['../classlib_1_1spt_1_1api.html#a8f4ac4109908607c0ebd16b9425b9ef9',1,'lib::spt::api']]],
-  ['display_5fname_36',['display_name',['../classlib_1_1spt_1_1user.html#a015858a71b0d9e7ea1c5fbf4739cea9e',1,'lib::spt::user']]],
-  ['duration_37',['duration',['../classlib_1_1spt_1_1track.html#a95db80e3f3255ceebda0e1d3a213166b',1,'lib::spt::track']]]
+  ['date_5ftime_28',['date_time',['../classlib_1_1date__time.html#a9a9097c65dacf644b2f6fcc12b61bd80',1,'lib::date_time::date_time()=default'],['../classlib_1_1date__time.html#a2321721ead30e1e788de467afbe9987c',1,'lib::date_time::date_time(int year, int month, int day, int hour, int minute, int second)'],['../classlib_1_1date__time.html#a5efb61cb61a126c74f122dc91fab60b3',1,'lib::date_time::date_time(const date_time &amp;date)'],['../classlib_1_1date__time.html',1,'lib::date_time']]],
+  ['decode_29',['decode',['../classlib_1_1base64.html#ae42ec5dcbc7448282b4f64909e95f31a',1,'lib::base64']]],
+  ['del_30',['del',['../classlib_1_1spt_1_1api.html#ae1c1d6b6d175f6897eee141d502ca313',1,'lib::spt::api::del(const std::string &amp;url, lib::callback&lt; std::string &gt; &amp;callback)'],['../classlib_1_1spt_1_1api.html#a71a727da67641b5a574086b05e1f1afb',1,'lib::spt::api::del(const std::string &amp;url, const nlohmann::json &amp;json, lib::callback&lt; std::string &gt; &amp;callback)=0']]],
+  ['deprecated_20list_31',['Deprecated List',['../deprecated.html',1,'']]],
+  ['description_32',['description',['../classlib_1_1spt_1_1playlist.html#af058c4eb3226f7b4d8cd270acdfa2514',1,'lib::spt::playlist']]],
+  ['dev_33',['dev',['../classlib_1_1log.html#a6faeebb4fdad6162e122a5941e8ae323',1,'lib::log::dev(const Format &amp;fmt, const Arg &amp;arg, Args &amp;&amp;... args)'],['../classlib_1_1log.html#af971c261aeceb74c0e806a9ea3e5867e',1,'lib::log::dev(const Format &amp;fmt)']]],
+  ['developer_5fmode_34',['developer_mode',['../classlib_1_1developer__mode.html',1,'lib']]],
+  ['device_35',['device',['../classlib_1_1spt_1_1playback.html#a347a1f37e20750a9ffaaf42cf93780bd',1,'lib::spt::playback::device()'],['../classlib_1_1spt_1_1device.html',1,'lib::spt::device']]],
+  ['devices_36',['devices',['../classlib_1_1spt_1_1api.html#a8f4ac4109908607c0ebd16b9425b9ef9',1,'lib::spt::api']]],
+  ['display_5fname_37',['display_name',['../classlib_1_1spt_1_1user.html#a015858a71b0d9e7ea1c5fbf4739cea9e',1,'lib::spt::user']]],
+  ['duration_38',['duration',['../classlib_1_1spt_1_1track.html#a95db80e3f3255ceebda0e1d3a213166b',1,'lib::spt::track']]]
 ];
--- search/functions_3.js
+++ search/functions_3.js
@@ -1,8 +1,7 @@
 var searchData=
 [
-  ['encode_226',['encode',['../classlib_1_1base64.html#a5dfb220d8028805107f9e9f36bb83004',1,'lib::base64']]],
-  ['ends_5fwith_227',['ends_with',['../classlib_1_1strings.html#a7a2cdbcb753e0b270abd87ce1e35a18d',1,'lib::strings']]],
-  ['erase_228',['erase',['../classlib_1_1vector.html#a221cd8e85cccd913ad3721bf75999233',1,'lib::vector']]],
+  ['encode_227',['encode',['../classlib_1_1base64.html#a5dfb220d8028805107f9e9f36bb83004',1,'lib::base64']]],
+  ['ends_5fwith_228',['ends_with',['../classlib_1_1strings.html#a7a2cdbcb753e0b270abd87ce1e35a18d',1,'lib::strings']]],
   ['error_229',['error',['../classlib_1_1log.html#aa35725a2fb8ba920c940cc019167f35a',1,'lib::log']]],
   ['error_5fmessage_230',['error_message',['../classlib_1_1spotify__error.html#aba0bbc5e036cfaf6a77bf5e3b3d8565d',1,'lib::spotify_error']]]
 ];
