2659492b92e087421fe0f8b0db248b87ed32e5e1
kraxarn <me@kraxarn.com>
2021-04-09 21:11:50 +0200

    Replace custom formatting with libfmt

--- lib/include/lib/format.hpp
+++ lib/include/lib/format.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#define FMT_HEADER_ONLY
+#include "thirdparty/fmt/format.h"
+
 #include "enum/logtype.hpp"
 
 #include <string>
@@ -9,33 +12,9 @@ namespace lib
 	/**
 	 * Formatting utilities
 	 */
-	class fmt
+	class format
 	{
 	public:
-		/**
-		 * Format a string as a string
-		 * @param fmt string
-		 * @return string
-		 */
-		template<typename Format>
-		static std::string format(const Format &fmt)
-		{
-			return fmt;
-		}
-
-		/**
-		 * Format a string with specified arguments as {} in the string
-		 * @param fmt string
-		 * @param arg Argument to to_string
-		 * @param args Arguments to to_string
-		 * @return Formatted string
-		 */
-		template<typename Format, typename Arg, typename... Args>
-		static std::string format(const Format &fmt, const Arg &arg, Args &&... args)
-		{
-			return format(collect(fmt, arg), args...);
-		}
-
 		/**
 		 * Format time as M:SS
 		 * @param ms Milliseconds
@@ -53,59 +32,5 @@ namespace lib
 		 * @param count Amount
 		 */
 		static auto count(unsigned int count) -> std::string;
-
-	private:
-		/**
-		 * Format string into another string
-		 * @param fmt String with {} to replace
-		 * @param arg String to replace {} with
-		 * @return Formatted string
-		 */
-		template<typename Format>
-		static std::string collect(const Format &fmt, const std::string &arg)
-		{
-			auto str = std::string(fmt);
-			auto index = str.find("{}");
-			if (index == std::string::npos)
-				return str;
-
-			return str.replace(index, 2, arg);
-		}
-
-		/**
-		 * Format value into a string
-		 * @param fmt String with {} to replace
-		 * @param arg Value to replace {} with
-		 * @return Formatted string
-		 */
-		template<typename Format, typename Arg>
-		static std::string collect(const Format &fmt, const Arg &arg)
-		{
-			return collect(fmt, std::to_string(arg));
-		}
-
-		/**
-		 * Format C-style string into a string
-		 * @param fmt String with {} to replace
-		 * @param arg C-style string to replace {} with
-		 * @return Formatted string
-		 */
-		template<typename Format>
-		static std::string collect(const Format &fmt, const char *arg)
-		{
-			return collect(fmt, std::string(arg));
-		}
-
-		/**
-		 * Format bool as "true" or "false"
-		 * @param fmt String with {} to replace
-		 * @param arg Value to replace {} with
-		 * @return Formatted string
-		 */
-		template<typename Format>
-		static std::string collect(const Format &fmt, bool arg)
-		{
-			return collect(fmt, arg ? "true" : "false");
-		}
 	};
 }
