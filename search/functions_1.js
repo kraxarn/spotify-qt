b2e0908b23a15c971da14a1f19a6f8ddadf86438
kraxarn <kraxarn@users.noreply.github.com>
2021-04-17 10:13:28 +0000

    deploy: 50ed58401e11fc88a0c038a74bfba976b7482c00

--- search/functions_1.js
+++ search/functions_1.js
@@ -4,7 +4,7 @@ var searchData=
   ['capitalize_214',['capitalize',['../classlib_1_1strings.html#a531ac77b4dc9cacd25bf7a4493bd62dd',1,'lib::strings']]],
   ['clear_215',['clear',['../classlib_1_1log.html#aac5793c9eb61c61b83bf738a75d90919',1,'lib::log']]],
   ['combine_216',['combine',['../classlib_1_1json.html#a4e21143b9ac2e5b2996ee8439a7a55af',1,'lib::json::combine()'],['../classlib_1_1vector.html#a9fbc23ff2283212ee4fb8676c7f30391',1,'lib::vector::combine()']]],
-  ['combine_5fnames_217',['combine_names',['../classlib_1_1spt_1_1entity.html#acd4636208a332f3d2f1f9f72e2ae2ec5',1,'lib::spt::entity']]],
+  ['combine_5fnames_217',['combine_names',['../classlib_1_1spt_1_1entity.html#acd4636208a332f3d2f1f9f72e2ae2ec5',1,'lib::spt::entity::combine_names(const std::vector&lt; entity &gt; &amp;entities) -&gt; std::string'],['../classlib_1_1spt_1_1entity.html#a4f5b7f3520cf211856e3268019a19890',1,'lib::spt::entity::combine_names(const std::vector&lt; entity &gt; &amp;entities, const char *separator) -&gt; std::string']]],
   ['config_5ffile_218',['config_file',['../classlib_1_1paths.html#a2c77312bf587b48fbb581e93730bec86',1,'lib::paths']]],
   ['contains_219',['contains',['../classlib_1_1set.html#a232a5db0b4b26d5c169cc22146b4a757',1,'lib::set::contains()'],['../classlib_1_1strings.html#ad6bb089dfe2793c7e01c1859e8c2b681',1,'lib::strings::contains()']]],
   ['count_220',['count',['../classlib_1_1fmt.html#a64de843df1a553d391314623db80b3c2',1,'lib::fmt']]],
