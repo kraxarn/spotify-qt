ab7fb0203df46fc5a500db4c3480d9dda611969e
kraxarn <me@kraxarn.com>
2021-04-18 10:51:16 +0200

    Moved client type to lib

---
+++ lib/include/lib/enum/clienttype.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace lib
+{
+	/**
+	 * Supported Spotify clients
+	 */
+	enum class client_type
+	{
+		/**
+		 * No client
+		 */
+		none,
+
+		/**
+		 * Spotifyd
+		 * @see https://github.com/Spotifyd/spotifyd
+		 */
+		spotifyd,
+
+		/**
+		 * librespot
+		 * @see https://github.com/librespot-org/librespot
+		 */
+		librespot
+	};
+}
--- src/enum/clienttype.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-enum class ClientType
-{
-	None,
-	Spotifyd,
-	Librespot
-};
