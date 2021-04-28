94f0d5b6911880d6790b5ef2c5510e30eb04aef8
kraxarn <me@kraxarn.com>
2021-04-28 22:24:25 +0200

    Only allow primary mouse button

--- src/dragarea.cpp
+++ src/dragarea.cpp
@@ -15,12 +15,22 @@ DragArea::DragArea(QWidget *parent)
 
 void DragArea::mousePressEvent(QMouseEvent *event)
 {
+	if (event->button() != Qt::LeftButton)
+	{
+		return QWidget::mousePressEvent(event);
+	}
+
 	dragPosition = event->globalPos() - mainWindow->frameGeometry().topLeft();
 	event->accept();
 }
 
 void DragArea::mouseMoveEvent(QMouseEvent *event)
 {
+	if (!(event->buttons() & Qt::LeftButton))
+	{
+		return QWidget::mouseMoveEvent(event);
+	}
+
 	setCursor(Qt::SizeAllCursor);
 	mainWindow->move(event->globalPos() - dragPosition);
 	event->accept();
