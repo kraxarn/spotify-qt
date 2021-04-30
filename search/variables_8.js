88d82e512c27988bea51fc61d8679478e4932e9e
kraxarn <kraxarn@users.noreply.github.com>
2021-04-30 20:17:32 +0000

    deploy: 197ad74e41327522469f2b57ff52c1599e61b562

--- search/variables_8.js
+++ search/variables_8.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['name_362',['name',['../classlib_1_1spt_1_1album.html#a1e3290ed717ea715afa1a672504ce95b',1,'lib::spt::album::name()'],['../classlib_1_1spt_1_1artist.html#ab2ac2371a7a286f91478a119c047afc5',1,'lib::spt::artist::name()'],['../classlib_1_1spt_1_1device.html#a125cf05de56ecb26be42cc6e40b5c1c8',1,'lib::spt::device::name()'],['../classlib_1_1spt_1_1entity.html#a52586eeb440239769c5383020a1ca50a',1,'lib::spt::entity::name()'],['../classlib_1_1spt_1_1playlist.html#af3c7ea64e6fa3fc9c1d9f6ee6e764b1e',1,'lib::spt::playlist::name()'],['../classlib_1_1spt_1_1playlist__details.html#a5e78dd53f231ed2dd5f91832ce9ece70',1,'lib::spt::playlist_details::name()']]]
+  ['name_362',['name',['../classlib_1_1spt_1_1device.html#a125cf05de56ecb26be42cc6e40b5c1c8',1,'lib::spt::device::name()'],['../classlib_1_1spt_1_1entity.html#a52586eeb440239769c5383020a1ca50a',1,'lib::spt::entity::name()'],['../classlib_1_1spt_1_1playlist.html#af3c7ea64e6fa3fc9c1d9f6ee6e764b1e',1,'lib::spt::playlist::name()'],['../classlib_1_1spt_1_1playlist__details.html#a5e78dd53f231ed2dd5f91832ce9ece70',1,'lib::spt::playlist_details::name()']]]
 ];
