7777757841f7e7034117f284510d24fb57f0adf6
kraxarn <me@kraxarn.com>
2021-04-30 21:20:01 +0200

    Revert "Remove data roles"
    This reverts commit 7c3c6b59

---
+++ src/enum/datarole.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+enum DataRole
+{
+	RoleTrackId = 0x0100,    // 256
+	RoleArtistId = 0x0101,   // 257
+	RoleAlbumId = 0x0102,    // 258
+	RoleIndex = 0x0103,      // 259
+	RolePlaylistId = 0x0104, // 260
+	RoleAddedDate = 0x105,   // 261
+	RoleLength = 0x106,      // 262
+	RoleArtists = 0x107,     // 263
+};
