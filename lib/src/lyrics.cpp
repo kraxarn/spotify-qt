bc4ae69db5fd4a4c8b6e7716655ba36289048587
kraxarn <me@kraxarn.com>
2021-05-10 20:56:28 +0200

    Fix never parsing lyrics

--- lib/src/lyrics.cpp
+++ lib/src/lyrics.cpp
@@ -49,7 +49,10 @@ void lib::lyrics::get(const spt::track &track, lib::result<std::string> &callbac
 auto lib::lyrics::format(const std::string &str) -> std::string
 {
 	auto val = lib::strings::replace_all(str, ' ', '-');
-	val = lib::strings::remove(val, "'");
+	val.erase(std::remove_if(val.begin(), val.end(), [](unsigned char c) -> bool
+	{
+		return c != '-' && std::isalpha(c) == 0;
+	}), val.end());
 	return val;
 }
 
@@ -68,7 +71,7 @@ auto lib::lyrics::get_from_lyrics(const std::string &lyrics) -> std::string
 	return lyrics.substr(start + start_str.size(), end);
 }
 
-auto lib::lyrics::get_from_lyrics_root(const std::string &lyrics) -> std::string
+auto lib::lyrics::get_from_lyrics_root(const std::string &/*lyrics*/) -> std::string
 {
 	return std::string();
 }
