cb8d12edb2912f47e70aba8306bc80296a6d7cd4
kraxarn <me@kraxarn.com>
2021-05-22 12:33:25 +0200

    Cleanup

--- lib/test/src/vectortests.cpp
+++ lib/test/src/vectortests.cpp
@@ -5,15 +5,19 @@
 
 TEST_CASE("vector")
 {
-	auto verify_eq = [](const std::vector<int> &v1, const std::vector<int> &v2)
+	auto verify_eq = [](const std::vector<int> &v1, const std::vector<int> &v2) -> bool
 	{
 		if (v1.size() != v2.size())
+		{
 			return false;
+		}
 
 		for (auto i = 0; i < v1.size(); i++)
 		{
 			if (v1.at(i) != v2.at(i))
+			{
 				return false;
+			}
 		}
 
 		return true;
