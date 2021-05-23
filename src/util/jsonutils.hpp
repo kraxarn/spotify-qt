14ba572ae323280f8cbc2b3ba7e1787f85f522b0
kraxarn <me@kraxarn.com>
2021-05-23 16:18:24 +0200

    Remove unused functions

--- src/util/jsonutils.hpp
+++ src/util/jsonutils.hpp
@@ -2,32 +2,12 @@
 
 #include "thirdparty/json.hpp"
 
-#include <QJsonValue>
-#include <QStringList>
-#include <QJsonObject>
-#include <QJsonArray>
 #include <QJsonDocument>
 #include <QVariantMap>
 
 class JsonUtils
 {
 public:
-	static QJsonValue getProperty(const QJsonObject &json, const QStringList &names);
-
-	template<typename T>
-	static QJsonArray toJsonArray(const QList<T> &list)
-	{
-		QJsonArray array;
-		for (auto &item : list)
-			array.append(item);
-		return array;
-	}
-
-	/**
-	 * Qt JSON -> C++ JSON
-	 */
-	static nlohmann::json toJson(const QJsonValue &json);
-
 	/**
 	 * C++ JSON item -> Qt JSON
 	 */
