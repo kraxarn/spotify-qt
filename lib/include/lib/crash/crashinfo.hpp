f299e546e837ebd29adf8a750977c4fe53eb9b8e
kraxarn <me@kraxarn.com>
2021-07-03 12:00:30 +0200

    Custom crash info class

---
+++ lib/include/lib/crash/crashinfo.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "thirdparty/json.hpp"
+
+#include <string>
+#include <unordered_map>
+
+namespace lib
+{
+	/**
+	 * Crash info with stack trace
+	 */
+	class crash_info
+	{
+	public:
+		/**
+		 * When did the crash occur
+		 */
+		long timestamp = 0;
+
+		/**
+		 * Crash signal
+		 */
+		int signal = -1;
+
+		/**
+		 * Stack trace with stack number
+		 */
+		std::unordered_map<int, std::string> stack_trace;
+
+		/**
+		 * Handled by application
+		 */
+		bool handled = false;
+	};
+
+	/** Crash info -> JSON */
+	void to_json(nlohmann::json &j, const crash_info &i);
+
+	/** JSON -> Crash info */
+	void from_json(const nlohmann::json &j, crash_info &i);
+}
---
+++ lib/src/crash/crashinfo.cpp
@@ -0,0 +1,24 @@
+#include "lib/crash/crashinfo.hpp"
+
+void lib::to_json(nlohmann::json &j, const crash_info &i)
+{
+	j = nlohmann::json{
+		{"timestamp", i.timestamp},
+		{"signal", i.signal},
+		{"stack_trace", i.stack_trace},
+		{"handled", i.handled},
+	}
+}
+
+void lib::from_json(const nlohmann::json &j, crash_info &i)
+{
+	if (!j.is_object())
+	{
+		return;
+	}
+
+	j.at("timestamp").get_to(i.timestamp);
+	j.at("signal").get_to(i.signal);
+	j.at("stack_trace").get_to(i.stack_trace);
+	j.at("handled").get_to(i.handled);
+}
