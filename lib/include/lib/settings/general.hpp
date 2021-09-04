007f58d87553c0323410ceec936ad90c0913f93c
kraxarn <kraxie@protonmail.com>
2021-09-04 17:41:45 +0200

    Revert "New keep track index setting"
    This reverts commit 483c21f6779fffe7d070169d6f632c686ef68fe4.

--- lib/include/lib/settings/general.hpp
+++ lib/include/lib/settings/general.hpp
@@ -133,11 +133,6 @@ namespace lib
 			 * ID of last selected device
 			 */
 			std::string last_device;
-
-			/**
-			 * Keep track index after re-sorting track list on another column
-			 */
-			bool keep_track_index = true;
 		};
 	}
 }
