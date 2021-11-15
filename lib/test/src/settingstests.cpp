7b7ff309e6a5e1a1eaaafe688660921ed28d6219
kraxarn <kraxie@protonmail.com>
2021-11-15 22:28:33 +0100

    Better tests for settings

--- lib/test/src/settingstests.cpp
+++ lib/test/src/settingstests.cpp
@@ -4,6 +4,9 @@
 #include "lib/paths/paths.hpp"
 #include "thirdparty/filesystem.hpp"
 
+#define CONFIG_FILE "/tmp/spotify-qt.json"
+#define CACHE_DIR "/tmp/spotify-qt/cache"
+
 class test_paths: public lib::paths
 {
 public:
@@ -19,20 +22,57 @@ public:
 
 	auto config_file() const -> ghc::filesystem::path override
 	{
-		return "spotify-qt.json";
+		return CONFIG_FILE;
 	}
 
 	auto cache() const -> ghc::filesystem::path override
 	{
-		return "cache";
+		return CACHE_DIR;
 	}
 };
 
 TEST_CASE("settings")
 {
 	test_paths paths;
-	lib::settings settings(paths);
 
-	settings.load();
-	settings.save();
+	auto read_settings = []() -> nlohmann::json
+	{
+		std::ifstream file(CONFIG_FILE);
+		nlohmann::json json;
+		file >> json;
+
+		return json;
+	};
+
+	auto create_settings = [&paths, &read_settings]() -> nlohmann::json
+	{
+		lib::settings settings(paths);
+		settings.save();
+
+		return read_settings();
+	};
+
+	SUBCASE("can create settings")
+	{
+		auto json = create_settings();
+
+		CHECK(json.at("Account").is_object());
+		CHECK(json.at("General").is_object());
+		CHECK(json.at("Spotify").is_object());
+	}
+
+	SUBCASE("qt is optional")
+	{
+		auto json = create_settings();
+
+		CHECK_FALSE(json.contains("Qt"));
+
+		lib::settings settings(paths);
+		settings.qt();
+		settings.save();
+
+		json = read_settings();
+		CHECK(json.contains("Qt"));
+		CHECK(json.at("Qt").is_object());
+	}
 }
