2b31e2a2f88fdb95d86ee0b18546fb392a6b4d94
kraxarn <kraxie@protonmail.com>
2021-12-11 11:57:59 +0100

    Remove using namespace

--- lib/src/spotifyapi/playlists.cpp
+++ lib/src/spotifyapi/playlists.cpp
@@ -1,9 +1,5 @@
 #include "lib/spotify/api.hpp"
 
-using namespace lib::spt;
-
-// Some calls are currently only available from the Playlist class
-
 // Currently unavailable:
 // users/{user_id}/playlists
 // users/{user_id}/playlists
@@ -22,25 +18,25 @@ void api::create_playlist(const std::string &name,
 		}, callback);
 }
 
-void api::playlists(lib::callback<std::vector<lib::spt::playlist>> &callback)
+void lib::spt::api::playlists(lib::callback<std::vector<lib::spt::playlist>> &callback)
 {
 	get_items("me/playlists?limit=50", callback);
 }
 
-void api::playlist(const std::string &playlist_id,
+void lib::spt::api::playlist(const std::string &playlist_id,
 	lib::callback<lib::spt::playlist> &callback)
 {
 	get(lib::fmt::format("playlists/{}", playlist_id), callback);
 }
 
-void api::edit_playlist(const std::string &playlist_id,
+void lib::spt::api::edit_playlist(const std::string &playlist_id,
 	const lib::spt::playlist_details &playlist,
 	lib::callback<std::string> &callback)
 {
 	put(lib::fmt::format("playlists/{}", playlist_id), playlist, callback);
 }
 
-void api::playlist_tracks(const lib::spt::playlist &playlist,
+void lib::spt::api::playlist_tracks(const lib::spt::playlist &playlist,
 	lib::callback<std::vector<lib::spt::track>> &callback)
 {
 	auto fetch = [this, callback](const std::string &url)
@@ -64,15 +60,16 @@ void api::playlist_tracks(const lib::spt::playlist &playlist,
 	}
 }
 
-void api::add_to_playlist(const std::string &playlist_id, const std::string &track_id,
+void lib::spt::api::add_to_playlist(const std::string &playlist_id, const std::string &track_id,
 	lib::callback<std::string> &callback)
 {
 	post(lib::fmt::format("playlists/{}/tracks?uris={}",
 		playlist_id, track_id), callback);
 }
 
-void api::remove_from_playlist(const std::string &playlist_id, const std::string &track_id,
-	int pos, lib::callback<std::string> &callback)
+void lib::spt::api::remove_from_playlist(const std::string &playlist_id,
+	const std::string &track_id, int pos,
+	lib::callback<std::string> &callback)
 {
 	del(lib::fmt::format("playlists/{}/tracks", playlist_id), {
 		{"tracks", {
