e2d1c370c88049b8b37d6a82ba91fcae61d15e57
kraxarn <kraxie@protonmail.com>
2022-01-18 21:55:29 +0100

    Encode artist name as uri safe

--- lib/src/search/api.cpp
+++ lib/src/search/api.cpp
@@ -1,4 +1,5 @@
 #include "lib/search/api.hpp"
+#include "lib/uri.hpp"
 
 lib::ddg::api::api(const lib::http_client &http_client)
 	: http(http_client)
@@ -8,10 +9,12 @@ lib::ddg::api::api(const lib::http_client &http_client)
 void lib::ddg::api::search(const lib::spt::artist &artist,
 	lib::callback<lib::ddg::results> &callback)
 {
+	const auto query = lib::uri::encode(artist.name);
+
 	const auto url = lib::fmt::format("https://api.duckduckgo.com"
 									  "?q={}"
 									  "&format=json"
-									  "&t=spotify-qt", artist.name);
+									  "&t=spotify-qt", query);
 
 	http.get(url, lib::headers(), [callback](const std::string &result)
 	{
