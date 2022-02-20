8d2ab01fba96fe498dd142b56d0180c26964ad0d
kraxarn <kraxie@protonmail.com>
2022-02-20 20:32:12 +0100

    Add font settings

--- lib/include/lib/settings/qt.hpp
+++ lib/include/lib/settings/qt.hpp
@@ -17,6 +17,17 @@ namespace lib
 			bool system_title_bar = false;
 
 			/**
+			 * Use a custom font instead of system default
+			 */
+			std::string custom_font_name;
+
+			/**
+			 * Use a custom font size instead of system default
+			 * @note 0 if not set
+			 */
+			int custom_font_size = 0;
+
+			/**
 			 * Mirror buttons in title bar
 			 * (requires system_title_bar to be false)
 			 */
