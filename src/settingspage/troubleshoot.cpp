75d420dff596caef5514b9688f7fca60b0aee098
kraxarn <kraxie@protonmail.com>
2022-02-26 17:28:10 +0100

    Developer mode toggle in settings

--- src/settingspage/troubleshoot.cpp
+++ src/settingspage/troubleshoot.cpp
@@ -1,4 +1,5 @@
 #include "settingspage/troubleshoot.hpp"
+#include "util/widget.hpp"
 
 SettingsPage::Troubleshoot::Troubleshoot(lib::settings &settings,
 	lib::cache &cache, QWidget *parent)
@@ -8,6 +9,7 @@ SettingsPage::Troubleshoot::Troubleshoot(lib::settings &settings,
 	addTab(systemInfo(), QStringLiteral("System"));
 	addTab(configPreview(), QStringLiteral("Config"));
 	addTab(cacheInfo(), QStringLiteral("Cache"));
+	addTab(developerMode(), QStringLiteral("Developer"));
 
 	if (lib::crash_handler::is_init()
 		|| !cache.get_all_crashes().empty())
@@ -33,6 +35,32 @@ auto SettingsPage::Troubleshoot::cacheInfo() -> QWidget *
 		paths = new QtPaths(this);
 	}
 	return new CacheView(*paths, this);
+}
+
+auto SettingsPage::Troubleshoot::developerMode() -> QWidget *
+{
+	auto *layout = tabContent();
+
+	auto *infoLabel = new QLabel(QStringLiteral(
+		"Enable developer mode to show more options and to help troubleshooting issues. "
+		"Developer mode is enabled until the application is closed."), this);
+
+	infoLabel->setWordWrap(true);
+	layout->addWidget(infoLabel);
+
+	auto *devMode = new QCheckBox(QStringLiteral("Developer mode"), this);
+	devMode->setChecked(lib::developer_mode::enabled);
+	layout->addWidget(devMode);
+
+	QCheckBox::connect(devMode, &QCheckBox::clicked,
+		this, &SettingsPage::Troubleshoot::onDevModeClicked);
+
+	return Widget::layoutToWidget(layout, this);
+}
+
+void SettingsPage::Troubleshoot::onDevModeClicked(bool checked)
+{
+	lib::developer_mode::enabled = checked;
 }
 
 auto SettingsPage::Troubleshoot::crashLogs() -> QWidget *
