646b22bd46cbb6b51b2528e732bd1e5b9c124410
kraxarn <kraxie@protonmail.com>
2022-04-24 20:37:35 +0200

    Initial unused icon check script

---
+++ scripts/unused_icons.py
@@ -0,0 +1,33 @@
+#!/bin/python
+# Checks for unused icons
+import os
+import pathlib
+
+
+def icon_dir() -> pathlib.Path:
+	return pathlib.Path.cwd().parent.joinpath("res", "ic")
+
+
+def source_dir() -> pathlib.Path:
+	return pathlib.Path.cwd().parent.joinpath("src")
+
+
+icon_names: list[str] = []
+for filename in os.listdir(os.path.join(icon_dir(), "dark")):
+	icon_names.append(pathlib.Path(filename).stem)
+total_count = len(icon_names)
+
+for root, dirs, files in os.walk(source_dir()):
+	root_path = pathlib.Path(root)
+	for filename in files:
+		file_path = root_path.joinpath(filename)
+		if file_path.suffix != ".cpp":
+			continue
+		with open(file_path, "r") as file:
+			for icon_name in icon_names:
+				if icon_name in file.read():
+					icon_names.remove(icon_name)
+
+count = len(icon_names)
+suffix = "icon" if count == 1 else "icons"
+print(f"Found {count}/{total_count} unused {suffix}")
