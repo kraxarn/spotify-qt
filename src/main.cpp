3f42b57845c23e7cff7429875d445f3efac2c8db
kraxarn <kraxie@protonmail.com>
2022-05-01 16:02:10 +0200

    New options for later

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,23 @@ auto commandLineOptions() -> QList<QCommandLineOption>
 			QStringLiteral("paths"),
 			QStringLiteral("Print paths for config file and cache."),
 		},
+		{
+			QStringLiteral("multiple-instances"),
+			QStringLiteral("Allow multiple instances, disables playback options."),
+		},
+		// Playback options
+		{
+			QStringLiteral("play-pause"),
+			QStringLiteral("Play, or pause, current track.")
+		},
+		{
+			QStringLiteral("previous"),
+			QStringLiteral("Go to the previous track"),
+		},
+		{
+			QStringLiteral("next"),
+			QStringLiteral("Go to the next track"),
+		},
 	};
 }
 
