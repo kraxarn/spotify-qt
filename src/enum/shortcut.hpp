6ee175e5df1642fb9b71746977ab894362a08d7e
kraxarn <kraxie@protonmail.com>
2022-06-06 16:37:46 +0200

    Fix Qt 6 build

--- src/enum/shortcut.hpp
+++ src/enum/shortcut.hpp
@@ -8,17 +8,17 @@
  */
 enum class Shortcut: int
 {
-	NewPlaylist = Qt::CTRL + Qt::Key_N,
+	NewPlaylist = Qt::CTRL | Qt::Key_N,
 	Delete = Qt::Key_Delete,
 
 	PlayPause = Qt::Key_Space,
-	Repeat = Qt::CTRL + Qt::Key_R,
-	Shuffle = Qt::CTRL + Qt::Key_S,
-	NextTrack = Qt::CTRL + Qt::Key_Right,
-	PreviousTrack = Qt::CTRL + Qt::Key_Left,
-	VolumeUp = Qt::CTRL + Qt::Key_Up,
-	VolumeDown = Qt::CTRL + Qt::Key_Down,
+	Repeat = Qt::CTRL | Qt::Key_R,
+	Shuffle = Qt::CTRL | Qt::Key_S,
+	NextTrack = Qt::CTRL | Qt::Key_Right,
+	PreviousTrack = Qt::CTRL | Qt::Key_Left,
+	VolumeUp = Qt::CTRL | Qt::Key_Up,
+	VolumeDown = Qt::CTRL | Qt::Key_Down,
 
 	PlaySelectedRow = Qt::Key_Return,
-	LogOut = Qt::CTRL + Qt::SHIFT + Qt::Key_W,
+	LogOut = Qt::CTRL | Qt::SHIFT | Qt::Key_W,
 };
