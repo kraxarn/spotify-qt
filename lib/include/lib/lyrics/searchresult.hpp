31c676602a056caa73057f5f61b680335a8019f2
kraxarn <kraxie@protonmail.com>
2022-08-20 14:26:14 +0200

    Add track, artists and album information

--- lib/include/lib/lyrics/searchresult.hpp
+++ lib/include/lib/lyrics/searchresult.hpp
@@ -16,6 +16,21 @@ namespace lib
 			 * Lyrics ID for fetching actual lyrics
 			 */
 			int lyrics_id;
+
+			/**
+			 * Name of track
+			 */
+			std::string track;
+
+			/**
+			 * Name of artists
+			 */
+			std::vector<std::string> artists;
+
+			/**
+			 * Name of album
+			 */
+			std::string album;
 		};
 
 		void from_json(const nlohmann::json &json, search_result &result);
--- lib/src/lyrics/searchresult.cpp
+++ lib/src/lyrics/searchresult.cpp
@@ -8,4 +8,18 @@ void lib::lrc::from_json(const nlohmann::json &json, lib::lrc::search_result &re
 	}
 
 	json.at("id").get_to(result.lyrics_id);
+	json.at("name").get_to(result.track);
+
+	if (json.contains("artists"))
+	{
+		for (const auto &artist: json.at("artists"))
+		{
+			result.artists.push_back(artist.at("name"));
+		}
+	}
+
+	if (json.contains("album"))
+	{
+		json.at("album").at("name").get_to(result.album);
+	}
 }
