34724bf644908816fb940da54b62845cf13c6445
kraxarn <kraxarn@users.noreply.github.com>
2022-08-20 14:45:03 +0000

    deploy: a33ab9eb2dcf9f8fdc2314ce577e67eb3530895b

--- search/all_4.js
+++ search/all_4.js
@@ -8,7 +8,8 @@ var searchData=
   ['enums_5',['enums',['../classlib_1_1enums.html',1,'lib']]],
   ['env_6',['env',['../classlib_1_1system.html#a569afc248641fbeeb5d52e72552c6d2a',1,'lib::system']]],
   ['episode_7',['episode',['../classlib_1_1spt_1_1episode.html',1,'lib::spt']]],
-  ['error_8',['error',['../classlib_1_1log.html#aa35725a2fb8ba920c940cc019167f35a',1,'lib::log::error()'],['../classlib_1_1spt_1_1error.html#a582c8784b6b2583f7bdf650c043648a2',1,'lib::spt::error::error(const char *what, const char *url)'],['../classlib_1_1spt_1_1error.html#a07c459200f2be13e1f3581ebdcf0d39e',1,'lib::spt::error::error(const std::string &amp;what, const std::string &amp;url)'],['../classlib_1_1spt_1_1error.html',1,'lib::spt::error']]],
-  ['error_5fmessage_9',['error_message',['../classlib_1_1spt_1_1error.html#aac5f0ee9aa261fd7a8cb1b76a499faee',1,'lib::spt::error']]],
-  ['external_5furls_10',['external_urls',['../classlib_1_1spt_1_1artist.html#a5c6d67c36d4f61358ec615aae7d8c6f5',1,'lib::spt::artist::external_urls()'],['../classlib_1_1spt_1_1episode.html#adb1b55fe03db2ab6bd1a29148f6050c5',1,'lib::spt::episode::external_urls()'],['../classlib_1_1spt_1_1show.html#a2fc2236541fde5a03dcd32e5e56fd609',1,'lib::spt::show::external_urls()']]]
+  ['erase_5fnon_5falpha_8',['erase_non_alpha',['../classlib_1_1strings.html#a9c4cb184ebdf1b92135ef7e4bc971f13',1,'lib::strings']]],
+  ['error_9',['error',['../classlib_1_1log.html#aa35725a2fb8ba920c940cc019167f35a',1,'lib::log::error()'],['../classlib_1_1spt_1_1error.html#a582c8784b6b2583f7bdf650c043648a2',1,'lib::spt::error::error(const char *what, const char *url)'],['../classlib_1_1spt_1_1error.html#a07c459200f2be13e1f3581ebdcf0d39e',1,'lib::spt::error::error(const std::string &amp;what, const std::string &amp;url)'],['../classlib_1_1spt_1_1error.html',1,'lib::spt::error']]],
+  ['error_5fmessage_10',['error_message',['../classlib_1_1spt_1_1error.html#aac5f0ee9aa261fd7a8cb1b76a499faee',1,'lib::spt::error']]],
+  ['external_5furls_11',['external_urls',['../classlib_1_1spt_1_1artist.html#a5c6d67c36d4f61358ec615aae7d8c6f5',1,'lib::spt::artist::external_urls()'],['../classlib_1_1spt_1_1episode.html#adb1b55fe03db2ab6bd1a29148f6050c5',1,'lib::spt::episode::external_urls()'],['../classlib_1_1spt_1_1show.html#a2fc2236541fde5a03dcd32e5e56fd609',1,'lib::spt::show::external_urls()']]]
 ];
--- search/functions_3.js
+++ search/functions_3.js
@@ -5,6 +5,7 @@ var searchData=
   ['ends_5fwith_2',['ends_with',['../classlib_1_1strings.html#a1020c4cf7a5e92f3d6ce405a48da9886',1,'lib::strings']]],
   ['entity_3',['entity',['../classlib_1_1spt_1_1entity.html#ad4fe6b9638d0291cdf6e212aa9ba926c',1,'lib::spt::entity']]],
   ['env_4',['env',['../classlib_1_1system.html#a569afc248641fbeeb5d52e72552c6d2a',1,'lib::system']]],
-  ['error_5',['error',['../classlib_1_1log.html#aa35725a2fb8ba920c940cc019167f35a',1,'lib::log::error()'],['../classlib_1_1spt_1_1error.html#a582c8784b6b2583f7bdf650c043648a2',1,'lib::spt::error::error(const char *what, const char *url)'],['../classlib_1_1spt_1_1error.html#a07c459200f2be13e1f3581ebdcf0d39e',1,'lib::spt::error::error(const std::string &amp;what, const std::string &amp;url)']]],
-  ['error_5fmessage_6',['error_message',['../classlib_1_1spt_1_1error.html#aac5f0ee9aa261fd7a8cb1b76a499faee',1,'lib::spt::error']]]
+  ['erase_5fnon_5falpha_5',['erase_non_alpha',['../classlib_1_1strings.html#a9c4cb184ebdf1b92135ef7e4bc971f13',1,'lib::strings']]],
+  ['error_6',['error',['../classlib_1_1log.html#aa35725a2fb8ba920c940cc019167f35a',1,'lib::log::error()'],['../classlib_1_1spt_1_1error.html#a582c8784b6b2583f7bdf650c043648a2',1,'lib::spt::error::error(const char *what, const char *url)'],['../classlib_1_1spt_1_1error.html#a07c459200f2be13e1f3581ebdcf0d39e',1,'lib::spt::error::error(const std::string &amp;what, const std::string &amp;url)']]],
+  ['error_5fmessage_7',['error_message',['../classlib_1_1spt_1_1error.html#aac5f0ee9aa261fd7a8cb1b76a499faee',1,'lib::spt::error']]]
 ];
