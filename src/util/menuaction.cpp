c1d43868a76223b4fcdc7a55b8a6df07df59c327
kraxarn <kraxie@protonmail.com>
2022-08-21 12:19:09 +0200

    Remove unnecessary overload

--- src/util/menuaction.cpp
+++ src/util/menuaction.cpp
@@ -1,14 +1,6 @@
 #include "util/menuaction.hpp"
 
 auto MenuAction::create(const QString &iconName, const QString &text,
-	QObject *parent, QKeySequence::StandardKey shortcut) -> QAction *
-{
-	auto *action = create(iconName, text, parent);
-	action->setShortcut(shortcut);
-	return action;
-}
-
-auto MenuAction::create(const QString &iconName, const QString &text,
 	QObject *parent, const QKeySequence &shortcut) -> QAction *
 {
 	auto *action = create(iconName, text, parent);
--- src/util/menuaction.hpp
+++ src/util/menuaction.hpp
@@ -10,12 +10,6 @@ class MenuAction
 {
 public:
 	/**
-	 * Create a new action with specified standard shortcut
-	 */
-	static auto create(const QString &iconName, const QString &text, QObject *parent,
-		QKeySequence::StandardKey shortcut) -> QAction *;
-
-	/**
 	 * Create a new action with specified application shortcut
 	 */
 	static auto create(const QString &iconName, const QString &text, QObject *parent,
