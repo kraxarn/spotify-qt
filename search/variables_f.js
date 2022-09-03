62c029b88f841a66acd90277a1d1747d4921bc11
kraxarn <kraxarn@users.noreply.github.com>
2022-09-03 15:24:55 +0000

    deploy: 08fc19486e5f30fefc17c676fe1c709ea53ea1b3

--- search/variables_f.js
+++ search/variables_f.js
@@ -1,12 +1,13 @@
 var searchData=
 [
-  ['settings_0',['settings',['../classlib_1_1spt_1_1api.html#a5a058e247f84895b9ee98f7b5a6ed756',1,'lib::spt::api']]],
-  ['shows_1',['shows',['../classlib_1_1spt_1_1search__results.html#a04606a5c13267ef4eef854de5f7088d7',1,'lib::spt::search_results']]],
-  ['shuffle_2',['shuffle',['../classlib_1_1spt_1_1playback.html#a593d145ce768c35439952a27ab29294d',1,'lib::spt::playback']]],
-  ['signal_3',['signal',['../classlib_1_1crash__info.html#a58bc7d2a0b45b541b913382fe0c3b57c',1,'lib::crash_info']]],
-  ['size_5flarge_4',['size_large',['../classlib_1_1spt_1_1image.html#ac20c45ee21888b0065a3c8914e21cd1b',1,'lib::spt::image']]],
-  ['size_5fsmall_5',['size_small',['../classlib_1_1spt_1_1image.html#a65016896f02a42915f896d2de663ca86',1,'lib::spt::image']]],
-  ['snapshot_6',['snapshot',['../classlib_1_1spt_1_1playlist.html#ae2eff6fa44f518622c1df2ecfe677f62',1,'lib::spt::playlist']]],
-  ['spotify_7',['spotify',['../classlib_1_1settings.html#a15cb6ff4193e655f271d8d76787c2507',1,'lib::settings']]],
-  ['stack_5ftrace_8',['stack_trace',['../classlib_1_1crash__info.html#a6470c0cfcaea6a053ddd9da81e626ef2',1,'lib::crash_info']]]
+  ['secs_5fin_5fmin_0',['secs_in_min',['../classlib_1_1format.html#aabb471cb7ade40881038d3c1b83a1b1a',1,'lib::format']]],
+  ['settings_1',['settings',['../classlib_1_1spt_1_1api.html#a5a058e247f84895b9ee98f7b5a6ed756',1,'lib::spt::api']]],
+  ['shows_2',['shows',['../classlib_1_1spt_1_1search__results.html#a04606a5c13267ef4eef854de5f7088d7',1,'lib::spt::search_results']]],
+  ['shuffle_3',['shuffle',['../classlib_1_1spt_1_1playback.html#a593d145ce768c35439952a27ab29294d',1,'lib::spt::playback']]],
+  ['signal_4',['signal',['../classlib_1_1crash__info.html#a58bc7d2a0b45b541b913382fe0c3b57c',1,'lib::crash_info']]],
+  ['size_5flarge_5',['size_large',['../classlib_1_1spt_1_1image.html#ac20c45ee21888b0065a3c8914e21cd1b',1,'lib::spt::image']]],
+  ['size_5fsmall_6',['size_small',['../classlib_1_1spt_1_1image.html#a65016896f02a42915f896d2de663ca86',1,'lib::spt::image']]],
+  ['snapshot_7',['snapshot',['../classlib_1_1spt_1_1playlist.html#ae2eff6fa44f518622c1df2ecfe677f62',1,'lib::spt::playlist']]],
+  ['spotify_8',['spotify',['../classlib_1_1settings.html#a15cb6ff4193e655f271d8d76787c2507',1,'lib::settings']]],
+  ['stack_5ftrace_9',['stack_trace',['../classlib_1_1crash__info.html#a6470c0cfcaea6a053ddd9da81e626ef2',1,'lib::crash_info']]]
 ];
