7e641eabc9e3d87d2b15454de756484d2bb8c985
kraxarn <kraxarn@users.noreply.github.com>
2022-09-10 10:37:56 +0000

    deploy: 769474121366eb95f4c0df98f34e6df58c761023

--- search/all_b.js
+++ search/all_b.js
@@ -4,6 +4,5 @@ var searchData=
   ['message_1',['message',['../classlib_1_1result.html#ad6ee5ee2e1f2dae434049ffdc72922cf',1,'lib::result']]],
   ['meta_2',['meta',['../classlib_1_1ddg_1_1results.html#a86159a01470dbdee97491e87c107a2fd',1,'lib::ddg::results']]],
   ['metadata_3',['metadata',['../classlib_1_1spt_1_1playback.html#a1f8a47dd2b4e30511d60ab7ec0ef2018',1,'lib::spt::playback']]],
-  ['ms_4',['ms',['../classlib_1_1stopwatch.html#a6657f6a36fd336c1a758695cad564033',1,'lib::stopwatch']]],
-  ['ms_5fin_5fsec_5',['ms_in_sec',['../classlib_1_1format.html#a39b4e1705ff941fcb81ac269f0d14e75',1,'lib::format']]]
+  ['ms_4',['ms',['../classlib_1_1stopwatch.html#a6657f6a36fd336c1a758695cad564033',1,'lib::stopwatch']]]
 ];
--- search/variables_a.js
+++ search/variables_a.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['media_5ftype_0',['media_type',['../classlib_1_1spt_1_1show.html#a9defc9290fd673dc484d20e678fbdb0a',1,'lib::spt::show']]],
-  ['meta_1',['meta',['../classlib_1_1ddg_1_1results.html#a86159a01470dbdee97491e87c107a2fd',1,'lib::ddg::results']]],
-  ['ms_5fin_5fsec_2',['ms_in_sec',['../classlib_1_1format.html#a39b4e1705ff941fcb81ac269f0d14e75',1,'lib::format']]]
+  ['meta_1',['meta',['../classlib_1_1ddg_1_1results.html#a86159a01470dbdee97491e87c107a2fd',1,'lib::ddg::results']]]
 ];
