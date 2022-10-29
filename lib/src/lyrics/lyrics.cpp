8d8a5e93ac9f08df3a96d2142da04bd30a9ef680
kraxarn <kraxie@protonmail.com>
2022-10-29 13:11:11 +0200

    Assume all initial : are credits

--- lib/src/lyrics/lyrics.cpp
+++ lib/src/lyrics/lyrics.cpp
@@ -11,18 +11,20 @@ void lib::lrc::from_json(const nlohmann::json &json, lib::lrc::lyrics &lyrics)
 	const auto lines = lib::strings::split(json, '\n');
 	lyrics.lines.reserve(lines.size());
 
-	for (const auto &line: lines)
+	auto iter = lines.cbegin();
+	while (iter != lines.cend())
 	{
-		const lib::lrc::line parsed(line);
-
-		if (lyrics.lines.empty()
-			&& (parsed.timestamp < 0 || parsed.timestamp % 1000L == 0)
-			&& parsed.text.find(':') != std::string::npos)
+		const lib::lrc::line parsed(*(iter++));
+		if (parsed.text.find(':') == std::string::npos)
 		{
-			lyrics.credits.emplace_back(parsed.text);
-			continue;
+			break;
 		}
+		lyrics.credits.emplace_back(parsed.text);
+	}
 
+	while (iter != lines.cend())
+	{
+		const lib::lrc::line parsed(*(iter++));
 		lyrics.lines.push_back(parsed);
 	}
 
