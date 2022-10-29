f1c88fa8fb49f1af6788ce94135fed65ef6764ce
kraxarn <kraxarn@users.noreply.github.com>
2022-10-29 12:23:57 +0000

    deploy: 0bca45c9c8ff1e469c7327a911e7d27c74a949fc

--- search/functions_12.js
+++ search/functions_12.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['uri_0',['uri',['../classlib_1_1uri.html#a235cffa04bad68e5eb27f84913b5242f',1,'lib::uri']]],
-  ['url_1',['url',['../classlib_1_1ddg_1_1link.html#aab2a1aa1829d32e8819ddc72d9ffb465',1,'lib::ddg::link::url()'],['../classlib_1_1spt_1_1auth.html#a3f4dd7f6fa15555e5ea65c11d8f50974',1,'lib::spt::auth::url()'],['../classlib_1_1spt_1_1error.html#a19860ef550fb939be1d47ec93f97142c',1,'lib::spt::error::url()']]]
+  ['unique_0',['unique',['../classlib_1_1vector.html#af0a0f3f4923cc18f96711a9b0bba7f19',1,'lib::vector']]],
+  ['uri_1',['uri',['../classlib_1_1uri.html#a235cffa04bad68e5eb27f84913b5242f',1,'lib::uri']]],
+  ['url_2',['url',['../classlib_1_1ddg_1_1link.html#aab2a1aa1829d32e8819ddc72d9ffb465',1,'lib::ddg::link::url()'],['../classlib_1_1spt_1_1auth.html#a3f4dd7f6fa15555e5ea65c11d8f50974',1,'lib::spt::auth::url()'],['../classlib_1_1spt_1_1error.html#a19860ef550fb939be1d47ec93f97142c',1,'lib::spt::error::url()']]]
 ];
