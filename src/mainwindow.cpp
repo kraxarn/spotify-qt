548568e2f1971bfc1b6b6a12dcbf8c612f185d28
devhyper <57877914+devhyper@users.noreply.github.com>
2022-11-23 15:48:29 -0800

    Add support for media keys on Windows

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -4,6 +4,10 @@
 #include "util/widget.hpp"
 #include "util/font.hpp"
 
+#ifdef Q_OS_WIN
+	#include "windows.h"
+#endif
+
 MainWindow::MainWindow(lib::settings &settings, lib::paths &paths,
 	lib::qt::http_client &httpClient, spt::Spotify &spotify)
 	: spotify(spotify),
@@ -78,9 +82,108 @@ void MainWindow::closeEvent(QCloseEvent *event)
 		event->accept();
 	}
 }
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0) 
+bool MainWindow::nativeEvent(const QByteArray &eventType, void *message, qintptr *result)
+#else
+bool MainWindow::nativeEvent(const QByteArray &eventType, void *message, long *result)
+#endif
+{
+	#ifdef Q_OS_WIN
+		MSG* msg = reinterpret_cast<MSG*>(message);
+		if (msg->message == WM_HOTKEY) {
+			UINT key = msg->wParam;
+			switch (key)
+			{
+				case 0: //Next Track
+				{
+					spotify.next([this](const std::string &status) 
+					{
+						if (!status.empty())
+						{
+							StatusMessage::error(QString("Failed to go to next track: %1")
+								.arg(QString::fromStdString(status)));
+							return;
+						}
+						refresh();
+					});
+					break;
+				}
+
+				case 1: //Previous Track
+				{
+					spotify.previous([this](const std::string &status) 
+					{
+						if (!status.empty())
+						{
+							StatusMessage::error(QString("Failed to go to previous track: %1")
+								.arg(QString::fromStdString(status)));
+							return;
+						}
+						refresh();
+					});
+					break;
+				}
+
+				case 2: //Stop Media
+				{
+					auto &current = getCurrentPlayback();
+					current.is_playing = false;
+					refreshed(current);
+					auto callback = [this](const std::string &status)
+					{
+						if (status.empty())
+						{
+							return;
+						}
+				
+						StatusMessage::error(QString("Failed to stop playback: %1")
+							.arg(QString::fromStdString(status)));
+					};
+					spotify.pause(callback);
+					break;
+				}
+
+				case 3: //Play/Pause Media
+				{
+					auto &current = getCurrentPlayback();
+					current.is_playing = !current.is_playing;
+					refreshed(current);
+					auto callback = [this](const std::string &status)
+					{
+						if (status.empty())
+						{
+							return;
+						}
+
+						StatusMessage::error(QString("Failed to play/pause playback: %1")
+							.arg(QString::fromStdString(status)));
+					};
+					if (current.is_playing)
+					{
+						spotify.resume(callback);
+					}
+					else
+					{
+						spotify.pause(callback);
+					}
+					break;
+				}
+			}
+		}
+	#endif
+	return QMainWindow::nativeEvent(eventType, message, result);
+}
 
 void MainWindow::initClient()
 {
+	#ifdef Q_OS_WIN
+		HWND hwnd = HWND(winId());
+		RegisterHotKey(hwnd, 0, MOD_NOREPEAT, 0xB0); //Next Track
+		RegisterHotKey(hwnd, 1, MOD_NOREPEAT, 0xB1); //Previous Track
+		RegisterHotKey(hwnd, 2, MOD_NOREPEAT, 0xB2); //Stop Media
+		RegisterHotKey(hwnd, 3, MOD_NOREPEAT, 0xB3); //Play/Pause Media
+	#endif
+
 	if (!settings.spotify.start_client)
 	{
 		return;
