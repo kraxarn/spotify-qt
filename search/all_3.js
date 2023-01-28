1451f23a3510297ca43e05166e2fe2066dd03021
kraxarn <kraxarn@users.noreply.github.com>
2023-01-28 11:50:20 +0000

    deploy: 155ff24145163cca0ca7216109283a368b4d2b60

--- search/all_3.js
+++ search/all_3.js
@@ -5,14 +5,15 @@ var searchData=
   ['date_5ftime_2',['date_time',['../classlib_1_1date__time.html#a9a9097c65dacf644b2f6fcc12b61bd80',1,'lib::date_time::date_time()=default'],['../classlib_1_1date__time.html#a2321721ead30e1e788de467afbe9987c',1,'lib::date_time::date_time(int year, int month, int day, int hour, int minute, int second)'],['../classlib_1_1date__time.html#a5efb61cb61a126c74f122dc91fab60b3',1,'lib::date_time::date_time(const date_time &amp;date)'],['../classlib_1_1date__time.html',1,'lib::date_time']]],
   ['debug_3',['debug',['../classlib_1_1log.html#a1999e6aa2a97ce318f453e91eeeac081',1,'lib::log::debug(const Format &amp;fmt, const Arg &amp;arg, Args &amp;&amp;... args)'],['../classlib_1_1log.html#aba6684a6930ae6b113cf42827473d9f4',1,'lib::log::debug(const Format &amp;fmt)']]],
   ['decode_4',['decode',['../classlib_1_1base64.html#a161b6e099d00016a9aed9150b0a70664',1,'lib::base64']]],
-  ['del_5',['del',['../classlib_1_1spt_1_1api.html#ae1c1d6b6d175f6897eee141d502ca313',1,'lib::spt::api::del()'],['../classlib_1_1http__client.html#ab4cc68e5a7ab4d9f140c80873f50ceca',1,'lib::http_client::del()'],['../classlib_1_1spt_1_1api.html#a3f97861134a29ce268d5730ef9c251a7',1,'lib::spt::api::del()']]],
+  ['del_5',['del',['../classlib_1_1spt_1_1api.html#ae1c1d6b6d175f6897eee141d502ca313',1,'lib::spt::api::del(const std::string &amp;url, lib::callback&lt; std::string &gt; &amp;callback)'],['../classlib_1_1spt_1_1api.html#a3f97861134a29ce268d5730ef9c251a7',1,'lib::spt::api::del(const std::string &amp;url, const nlohmann::json &amp;json, lib::callback&lt; std::string &gt; &amp;callback)'],['../classlib_1_1http__client.html#ab4cc68e5a7ab4d9f140c80873f50ceca',1,'lib::http_client::del()']]],
   ['deprecated_20list_6',['Deprecated List',['../deprecated.html',1,'']]],
   ['description_7',['description',['../classlib_1_1spt_1_1episode.html#a1da6dd18795dd06dc52584d787b1e935',1,'lib::spt::episode::description()'],['../classlib_1_1spt_1_1playlist.html#af058c4eb3226f7b4d8cd270acdfa2514',1,'lib::spt::playlist::description()'],['../classlib_1_1spt_1_1playlist__details.html#a920cfc501f831c61e01fb0fa7cfcb812',1,'lib::spt::playlist_details::description()'],['../classlib_1_1spt_1_1show.html#a91c835addf663d101cb82d35cc33e6ef',1,'lib::spt::show::description()']]],
   ['details_8',['details',['../classlib_1_1spt_1_1track.html#abc6da82e3f541950f2293c7da4ed18b3',1,'lib::spt::track']]],
   ['developer_5fmode_9',['developer_mode',['../classlib_1_1developer__mode.html',1,'lib']]],
   ['device_10',['device',['../classlib_1_1spt_1_1playback.html#a347a1f37e20750a9ffaaf42cf93780bd',1,'lib::spt::playback::device()'],['../classlib_1_1spt_1_1device.html',1,'lib::spt::device']]],
-  ['devices_11',['devices',['../classlib_1_1spt_1_1api.html#ab71b9c21944c8b1ae9f2c6a78ff99cb6',1,'lib::spt::api']]],
-  ['display_5fname_12',['display_name',['../classlib_1_1spt_1_1user.html#a015858a71b0d9e7ea1c5fbf4739cea9e',1,'lib::spt::user']]],
-  ['duration_13',['duration',['../classlib_1_1spt_1_1track.html#a95db80e3f3255ceebda0e1d3a213166b',1,'lib::spt::track']]],
-  ['duration_5fms_14',['duration_ms',['../classlib_1_1spt_1_1episode.html#af82863fd6e7b6f6994c5c341b0b4505f',1,'lib::spt::episode']]]
+  ['device_5fselect_11',['device_select',['../classlib_1_1spt_1_1device__select.html',1,'lib::spt']]],
+  ['devices_12',['devices',['../classlib_1_1spt_1_1api.html#ab71b9c21944c8b1ae9f2c6a78ff99cb6',1,'lib::spt::api']]],
+  ['display_5fname_13',['display_name',['../classlib_1_1spt_1_1user.html#a015858a71b0d9e7ea1c5fbf4739cea9e',1,'lib::spt::user']]],
+  ['duration_14',['duration',['../classlib_1_1spt_1_1track.html#a95db80e3f3255ceebda0e1d3a213166b',1,'lib::spt::track']]],
+  ['duration_5fms_15',['duration_ms',['../classlib_1_1spt_1_1episode.html#af82863fd6e7b6f6994c5c341b0b4505f',1,'lib::spt::episode']]]
 ];
--- search/classes_3.js
+++ search/classes_3.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['date_5ftime_0',['date_time',['../classlib_1_1date__time.html',1,'lib']]],
   ['developer_5fmode_1',['developer_mode',['../classlib_1_1developer__mode.html',1,'lib']]],
-  ['device_2',['device',['../classlib_1_1spt_1_1device.html',1,'lib::spt']]]
+  ['device_2',['device',['../classlib_1_1spt_1_1device.html',1,'lib::spt']]],
+  ['device_5fselect_3',['device_select',['../classlib_1_1spt_1_1device__select.html',1,'lib::spt']]]
 ];
