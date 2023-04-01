39a166dcba11595172893f9b0c13e6fc5b88e55f
kraxarn <kraxie@protonmail.com>
2023-04-01 17:45:44 +0200

    Initial history button

---
+++ src/widget/historybutton.cpp
@@ -0,0 +1,9 @@
+#include "widget/historybutton.hpp"
+
+HistoryButton::HistoryButton(QWidget *parent)
+	: QAction(parent)
+{
+	setIcon(Icon::get(QStringLiteral("go-previous")));
+	setText(QStringLiteral("Go back"));
+	setEnabled(false);
+}
---
+++ src/widget/historybutton.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "util/icon.hpp"
+
+#include <QAction>
+
+class HistoryButton: public QAction
+{
+public:
+	explicit HistoryButton(QWidget *parent);
+};
