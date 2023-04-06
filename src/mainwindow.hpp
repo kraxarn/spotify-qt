9e6fd0dd916dddc6cef77762b1fb314742ba976d
kraxarn <kraxie@protonmail.com>
2023-04-06 21:21:22 +0200

    Fix whitespace

--- src/mainwindow.hpp
+++ src/mainwindow.hpp
@@ -79,7 +79,7 @@ signals:
 
 protected:
 	void closeEvent(QCloseEvent *event) override;
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0) 
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
 	bool nativeEvent(const QByteArray &eventType, void *message, qintptr *result) override;
 #else
 	bool nativeEvent(const QByteArray &eventType, void *message, long *result) override;
