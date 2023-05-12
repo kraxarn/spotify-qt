d7d187cff797adbeb4ab99ee3bbd2d1cb7f308ef
kraxarn <kraxie@protonmail.com>
2023-05-12 21:57:01 +0200

    Fix MinGW build

--- lib/test/src/stringstests.cpp
+++ lib/test/src/stringstests.cpp
@@ -132,7 +132,8 @@ TEST_CASE("strings")
 
 		// Overflow
 		i = 0;
-		CHECK_THROWS(lib::strings::try_to_int(std::to_string(INT64_MAX), i));
+		const auto long_max = std::numeric_limits<long>::max();
+		CHECK_THROWS(lib::strings::try_to_int(std::to_string(long_max), i));
 		CHECK_EQ(i, 0);
 	}
 
