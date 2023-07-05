b46954c8468e21ad4b523ea0ea80be510d5698a0
kraxarn <kraxie@protonmail.com>
2023-07-05 16:15:08 +0200

    Fix not applying flag

--- lib/src/developermode.cpp
+++ lib/src/developermode.cpp
@@ -6,7 +6,7 @@ unsigned short lib::developer_mode::experiments = 0;
 void lib::developer_mode::add_experiment(lib::experiment experiment)
 {
 	const auto value = static_cast<unsigned short>(experiment);
-	lib::developer_mode::experiments &= value;
+	lib::developer_mode::experiments |= value;
 }
 
 auto lib::developer_mode::is_experiment_enabled(lib::experiment experiment) -> bool
