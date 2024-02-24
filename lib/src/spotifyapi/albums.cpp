edce0eb6c9e395db39c38a5e6b3898e554e0c016
kraxarn <kraxie@protonmail.com>
2024-02-24 18:52:16 +0100

    Remove old method

--- lib/src/spotifyapi/albums.cpp
+++ lib/src/spotifyapi/albums.cpp
@@ -5,24 +5,6 @@ void lib::spt::api::album(const std::string &id, lib::callback<lib::spt::album> 
 	get(lib::fmt::format("albums/{}", id), callback);
 }
 
-void lib::spt::api::album_tracks(const lib::spt::album &album,
-	lib::callback<std::vector<lib::spt::track>> &callback)
-{
-	get_items(lib::fmt::format("albums/{}/tracks?limit=50", album.id),
-		[album, callback](const std::vector<lib::spt::track> &results)
-		{
-			std::vector<lib::spt::track> tracks;
-			tracks.reserve(results.size());
-			for (const auto &result: results)
-			{
-				lib::spt::track track = result;
-				track.album.name = album.name;
-				tracks.push_back(track);
-			}
-			callback(tracks);
-		});
-}
-
 void lib::spt::api::album_tracks(const spt::album &album, const paged_callback<spt::track> &callback) const
 {
 	const auto albumName = album.name;
