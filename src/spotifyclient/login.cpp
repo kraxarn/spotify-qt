35e8516d22ad1424d98b55c5bea279b24e19f787
kraxarn <kraxie@protonmail.com>
2024-11-16 18:26:52 +0100

    Initial login helper

---
+++ src/spotifyclient/login.cpp
@@ -0,0 +1,76 @@
+#include "spotifyclient/login.hpp"
+
+#include "spotifyclient/helper.hpp"
+#include "util/url.hpp"
+
+#include <QObject>
+#include <QProcess>
+
+SpotifyClient::Login::Login(QWidget *parent)
+	: QObject(parent),
+	process(new QProcess(this))
+{
+	connect(process, &QProcess::readyReadStandardOutput,
+		this, &Login::onReadyReadOutput);
+
+	connect(process, &QProcess::readyReadStandardError,
+		this, &Login::onReadyReadError);
+
+	connect(process, &QProcess::errorOccurred,
+		this, &Login::onErrorOccurred);
+}
+
+void SpotifyClient::Login::run(const QString &path)
+{
+	if (!Helper::getOAuthSupport(path))
+	{
+		emit loginFailed(QStringLiteral("Client unsupported"));
+		return;
+	}
+
+	const QStringList arguments({
+		"--enable-oath"
+	});
+
+	process->start(path, arguments);
+}
+
+void SpotifyClient::Login::onReadyReadOutput()
+{
+	const auto output = process->readAllStandardOutput();
+
+	for (auto &line: QString(output).split('\n'))
+	{
+		if (line.isEmpty())
+		{
+			continue;
+		}
+
+		const auto urlIndex = line.indexOf(QStringLiteral("https://accounts.spotify.com/authorize"));
+		if (urlIndex >= 0)
+		{
+			const auto url = line.right(line.length() - urlIndex - 1);
+			auto *parent = qobject_cast<QWidget *>(QObject::parent());
+			Url::open(url, LinkType::Web, parent);
+			continue;
+		}
+
+		if (line.contains(QStringLiteral("Authenticated")))
+		{
+			emit loginSuccess();
+			return;
+		}
+	}
+}
+
+void SpotifyClient::Login::onReadyReadError()
+{
+	const auto output = process->readAllStandardError();
+	emit loginFailed(QString(output));
+}
+
+void SpotifyClient::Login::onErrorOccurred(const QProcess::ProcessError error)
+{
+	const auto message = Helper::processErrorToString(error);
+	emit loginFailed(message);
+}
---
+++ src/spotifyclient/login.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <QObject>
+#include <QProcess>
+#include <QWidget>
+
+namespace SpotifyClient
+{
+	class Login final: public QObject
+	{
+	Q_OBJECT
+
+	public:
+		explicit Login(QWidget *parent);
+
+		void run(const QString &path);
+
+	signals:
+		void loginSuccess();
+		void loginFailed(const QString &message);
+
+	private:
+		QProcess *process;
+
+		void onReadyReadOutput();
+		void onReadyReadError();
+		void onErrorOccurred(QProcess::ProcessError error);
+	};
+}
