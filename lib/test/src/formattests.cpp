04b7e435616ecab8e139b5dc601d7e85d80f9233
kraxarn <kraxie@protonmail.com>
2026-02-08 20:41:50 +0100

    Update tests

--- lib/test/src/formattests.cpp
+++ lib/test/src/formattests.cpp
@@ -3,31 +3,31 @@
 #include "lib/spotify/artist.hpp"
 #include "lib/spotify/track.hpp"
 
-TEST_CASE("fmt::format")
+TEST_CASE("Format")
 {
 	SUBCASE("time")
 	{
-		CHECK_EQ(lib::format::time(0), "0:00");
-		CHECK_EQ(lib::format::time(999), "0:00");
-		CHECK_EQ(lib::format::time(5000), "0:05");
-		CHECK_EQ(lib::format::time(60 * 1000), "1:00");
-		CHECK_EQ(lib::format::time(65 * 1000), "1:05");
+		CHECK_EQ(Format::time(0), "0:00");
+		CHECK_EQ(Format::time(999), "0:00");
+		CHECK_EQ(Format::time(5000), "0:05");
+		CHECK_EQ(Format::time(60 * 1000), "1:00");
+		CHECK_EQ(Format::time(65 * 1000), "1:05");
 	}
 
-	SUBCASE("time_pretty")
+	SUBCASE("timePretty")
 	{
-		CHECK_EQ(lib::format::time_pretty(0), "0 m");
-		CHECK_EQ(lib::format::time_pretty(59 * 1000 * 60), "59 m");
-		CHECK_EQ(lib::format::time_pretty(60 * 1000 * 60), "1 h");
-		CHECK_EQ(lib::format::time_pretty(61 * 1000 * 60), "1 h 1 m");
+		CHECK_EQ(Format::timePretty(0), "0 m");
+		CHECK_EQ(Format::timePretty(59 * 1000 * 60), "59 m");
+		CHECK_EQ(Format::timePretty(60 * 1000 * 60), "1 h");
+		CHECK_EQ(Format::timePretty(61 * 1000 * 60), "1 h 1 m");
 	}
 
 	SUBCASE("size")
 	{
-		CHECK_EQ(lib::format::size(1), "1 B");
-		CHECK_EQ(lib::format::size(1000), "1 kB");
-		CHECK_EQ(lib::format::size(1000000), "1 MB");
-		CHECK_EQ(lib::format::size(1000000000), "1 GB");
+		CHECK_EQ(Format::size(1), "1 B");
+		CHECK_EQ(Format::size(1000), "1 kB");
+		CHECK_EQ(Format::size(1000000), "1 MB");
+		CHECK_EQ(Format::size(1000000000), "1 GB");
 	}
 
 	SUBCASE("title")
@@ -43,14 +43,14 @@ TEST_CASE("fmt::format")
 		artist2.name = "artist2";
 		track.artists.push_back(artist2);
 
-		CHECK_EQ(lib::format::title(track, "{track}"), "track");
-		CHECK_EQ(lib::format::title(track, "{track"), "{track");
-		CHECK_EQ(lib::format::title(track, "prefix - {track}"), "prefix - track");
-		CHECK_EQ(lib::format::title(track, "{track} - suffix"), "track - suffix");
-		CHECK_EQ(lib::format::title(track, "{track} - {invalid}"), "track - {invalid}");
+		CHECK_EQ(Format::title(track, "{track}"), "track");
+		CHECK_EQ(Format::title(track, "{track"), "{track");
+		CHECK_EQ(Format::title(track, "prefix - {track}"), "prefix - track");
+		CHECK_EQ(Format::title(track, "{track} - suffix"), "track - suffix");
+		CHECK_EQ(Format::title(track, "{track} - {invalid}"), "track - {invalid}");
 
-		CHECK_EQ(lib::format::title(track, "{artist} - {track}"), "artist1 - track");
-		CHECK_EQ(lib::format::title(track, "{artists} - {track}"), "artist1, artist2 - track");
+		CHECK_EQ(Format::title(track, "{artist} - {track}"), "artist1 - track");
+		CHECK_EQ(Format::title(track, "{artists} - {track}"), "artist1, artist2 - track");
 
 		CHECK_EQ(lib::format::title(track, ""), track.title());
 	}
