fce55ae632cbf7b933b6dcd216e9c8fa1c8e030d
kraxarn <kraxie@protonmail.com>
2026-02-15 01:16:22 +0100

    Remove all other unused properties

--- lib/include/lib/spotify/episode.hpp
+++ lib/include/lib/spotify/episode.hpp
@@ -16,20 +16,9 @@ namespace lib
 		{
 		public:
 			/**
-			 * Description in plain text
-			 */
-			std::string description;
-
-			/**
 			 * Length in milliseconds
 			 */
 			int duration_ms = 0;
-
-			/**
-			 * If episode has explicit content
-			 * @note false if unknown
-			 */
-			bool is_explicit = false;
 
 			/**
 			 * External URLs
@@ -37,24 +26,9 @@ namespace lib
 			std::map<std::string, std::string> external_urls;
 
 			/**
-			 * Link ot the Web API endpoint
-			 */
-			std::string href;
-
-			/**
-			 * Description in HTML
-			 */
-			std::string html_description;
-
-			/**
 			 * URLs to cover art
 			 */
 			std::vector<lib::spt::image> images;
-
-			/**
-			 * Hosted outside of Spotify's CDN
-			 */
-			bool is_externally_hosted = false;
 
 			/**
 			 * Playable in current market
@@ -62,25 +36,10 @@ namespace lib
 			bool is_playable = false;
 
 			/**
-			 * Languages used in ISO 639 codes
-			 */
-			std::vector<std::string> languages;
-
-			/**
 			 * Date when first released
 			 * @note Varying precision
 			 */
 			std::string release_date;
-
-			/**
-			 * Precision of release_date
-			 */
-			std::string release_date_precision;
-
-			/**
-			 * Spotify URI
-			 */
-			std::string uri;
 
 			/**
 			 * Convert to track entity, mostly for compatibility
--- lib/src/spotify/episode.cpp
+++ lib/src/spotify/episode.cpp
@@ -7,21 +7,13 @@ void lib::spt::from_json(const nlohmann::json &j, episode &e)
 		return;
 	}
 
-	j.at("description").get_to(e.description);
 	j.at("duration_ms").get_to(e.duration_ms);
-	j.at("explicit").get_to(e.is_explicit);
 	j.at("external_urls").get_to(e.external_urls);
-	j.at("href").get_to(e.href);
-	j.at("html_description").get_to(e.html_description);
 	j.at("id").get_to(e.id);
 	j.at("images").get_to(e.images);
-	j.at("is_externally_hosted").get_to(e.is_externally_hosted);
 	j.at("is_playable").get_to(e.is_playable);
-	j.at("languages").get_to(e.languages);
 	j.at("name").get_to(e.name);
 	j.at("release_date").get_to(e.release_date);
-	j.at("release_date_precision").get_to(e.release_date_precision);
-	j.at("uri").get_to(e.uri);
 }
 
 auto lib::spt::episode::to_track(const lib::spt::show &show) const -> lib::spt::track
