728e55581f4024b10ddc99c9e993d2e99b468730
kraxarn <kraxarn@users.noreply.github.com>
2026-03-05 17:23:31 +0000

    deploy: 1c5dbf1ac0e62773b3e5a2a2b9d6cfda4207db8b

--- search/all_14.js
+++ search/all_14.js
@@ -3,6 +3,7 @@ var searchData=
   ['value_0',['value',['../classlib_1_1ddg_1_1link.html#a1bdc7b4a2715a4c1bc5d08ce6e9cbca3',1,'lib::ddg::link::value'],['../classResult.html#aff06577274f166c1aa501f3c5ed767e7',1,'Result::value()']]],
   ['vector_1',['vector',['../classlib_1_1vector.html',1,'lib']]],
   ['version_2',['version',['../classlib_1_1spt_1_1playlist.html#a57af1a5f60dd8e356a4568f624d286cc',1,'lib::spt::playlist']]],
-  ['volume_3',['volume',['../classlib_1_1spt_1_1playback.html#a788f323394b808b2698413d1460d9b8c',1,'lib::spt::playback']]],
-  ['volume_5fpercent_4',['volume_percent',['../classlib_1_1spt_1_1device.html#a4660cde6df9a480e5594d8252413140b',1,'lib::spt::device']]]
+  ['void_3',['Void',['../classVoid.html',1,'']]],
+  ['volume_4',['volume',['../classlib_1_1spt_1_1playback.html#a788f323394b808b2698413d1460d9b8c',1,'lib::spt::playback']]],
+  ['volume_5fpercent_5',['volume_percent',['../classlib_1_1spt_1_1device.html#a4660cde6df9a480e5594d8252413140b',1,'lib::spt::device']]]
 ];
--- search/classes_10.js
+++ search/classes_10.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['vector_0',['vector',['../classlib_1_1vector.html',1,'lib']]]
+  ['vector_0',['vector',['../classlib_1_1vector.html',1,'lib']]],
+  ['void_1',['Void',['../classVoid.html',1,'']]]
 ];
