a20aa4e90c9f2aeccb00c74aa7205f5c4c98d7fb
kraxarn <kraxarn@users.noreply.github.com>
2026-05-14 14:24:30 +0000

    deploy: e48c4ff84146056e960b5d38b6ed2e991e1d6e8b

--- search/classes_d.js
+++ search/classes_d.js
@@ -5,10 +5,11 @@ var searchData=
   ['set_2',['set',['../classlib_1_1set.html',1,'lib']]],
   ['settings_3',['settings',['../classlib_1_1settings.html',1,'lib']]],
   ['show_4',['show',['../classlib_1_1spt_1_1show.html',1,'lib::spt']]],
-  ['spotifyerrorutil_5',['SpotifyErrorUtil',['../classSpotifyErrorUtil.html',1,'']]],
-  ['spotifysaveditems_6',['SpotifySavedItems',['../classSpotifySavedItems.html',1,'']]],
-  ['stopwatch_7',['stopwatch',['../classlib_1_1stopwatch.html',1,'lib']]],
-  ['strings_8',['strings',['../classlib_1_1strings.html',1,'lib']]],
-  ['system_9',['system',['../classlib_1_1system.html',1,'lib']]],
-  ['system_5finfo_10',['system_info',['../classlib_1_1qt_1_1system__info.html',1,'lib::qt']]]
+  ['spotifyappstatus_5',['SpotifyAppStatus',['../classSpotifyAppStatus.html',1,'']]],
+  ['spotifyerrorutil_6',['SpotifyErrorUtil',['../classSpotifyErrorUtil.html',1,'']]],
+  ['spotifysaveditems_7',['SpotifySavedItems',['../classSpotifySavedItems.html',1,'']]],
+  ['stopwatch_8',['stopwatch',['../classlib_1_1stopwatch.html',1,'lib']]],
+  ['strings_9',['strings',['../classlib_1_1strings.html',1,'lib']]],
+  ['system_10',['system',['../classlib_1_1system.html',1,'lib']]],
+  ['system_5finfo_11',['system_info',['../classlib_1_1qt_1_1system__info.html',1,'lib::qt']]]
 ];
